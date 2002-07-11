d64d2483dbc5bafd1095d7b07ac06de2cc4ec959
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2002-07-11 22:34:09 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@31 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/actions/UpdateDatabaseAction.java
@@ -0,0 +1,40 @@
+/*
+ * ClearAction.java
+ *
+ * Created on December 2, 2001, 1:32 AM
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+import workbench.gui.display.SqlPanel;
+import workbench.interfaces.ClipboardSupport;
+import workbench.interfaces.Exporter;
+import workbench.resource.ResourceMgr;
+
+/**
+ *	Action to copy the contents of a entry field into the clipboard
+ *	@author  thomas.kellerer@web.de
+ */
+public class UpdateDatabaseAction extends AbstractAction
+{
+	private SqlPanel panel;
+	
+	public UpdateDatabaseAction(SqlPanel aPanel)
+	{
+		this.panel = aPanel;
+		this.putValue(Action.NAME, ResourceMgr.getString("UpdateDatabase"));
+		this.putValue(Action.SHORT_DESCRIPTION, ResourceMgr.getDescription("UpdateDatabase"));
+		this.putValue(Action.SMALL_ICON, ResourceMgr.getImage(ResourceMgr.IMG_SAVE));
+		this.putValue(WbActionConstants.MAIN_MENU_ITEM, ResourceMgr.MNU_TXT_DATA);
+		this.putValue(WbActionConstants.ADD_TO_TOOLBAR, "false");
+		this.putValue(WbActionConstants.TBAR_SEPARATOR, "false");
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		this.panel.saveChangesToDatabase();
+	}
+}
