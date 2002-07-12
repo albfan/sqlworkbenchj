a36ca28c3683bff6e3cd1e55ba51666aed44f461
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2002-07-12 21:26:17 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@34 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/gui/components/WbTableSorter.java
+++ src/workbench/gui/components/WbTableSorter.java
@@ -1,58 +1,13 @@
-/*-- 
-
- Copyright (C) 2000-2002 Anthony Eden.
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
- 
- 1. Redistributions of source code must retain the above copyright
-    notice, this list of conditions, and the following disclaimer.
- 
- 2. Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions, and the disclaimer that follows 
-    these conditions in the documentation and/or other materials 
-    provided with the distribution.
-
- 3. The name "EdenLib" must not be used to endorse or promote products
-    derived from this software without prior written permission.  For
-    written permission, please contact me@anthonyeden.com.
- 
- 4. Products derived from this software may not be called "EdenLib", nor
-    may "EdenLib" appear in their name, without prior written permission
-    from Anthony Eden (me@anthonyeden.com).
- 
- In addition, I request (but do not require) that you include in the 
- end-user documentation provided with the redistribution and/or in the 
- software itself an acknowledgement equivalent to the following:
-     "This product includes software developed by
-      Anthony Eden (http://www.anthonyeden.com/)."
-
- THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, 
- INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
- STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
- POSSIBILITY OF SUCH DAMAGE.
-
- For more information on EdenLib, please see <http://edenlib.sf.net/>.
- 
- */
-
 package workbench.gui.components;
 
+import java.awt.Cursor;
 import java.awt.event.InputEvent;
 // Imports for picking up mouse events from the JTable.
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import java.util.*;
+import java.util.Arrays;
 
 import javax.swing.JTable;
 import javax.swing.event.TableModelEvent;
@@ -61,34 +16,25 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
+import workbench.gui.WbSwingUtilities;
 
 
-/**
- * A sorter for TableModels. The sorter has a model (conforming to TableModel)
- * and itself implements TableModel. TableSorter does not store or copy
- * the data in the TableModel, instead it maintains an array of
- * integers which it keeps the same size as the number of rows in its
- * model. When the model changes it notifies the sorter that something
- * has changed eg. "rowsAdded" so that its internal array of integers
- * can be reallocated. As requests are made of the sorter (like
- * getValueAt(row, col) it redirects them to its model via the mapping
- * array. That way the TableSorter appears to hold another copy of the table
- * with the rows in a different order. The sorting algorthm used is stable
- * which means that it does not move around rows when its comparison
- * function returns 0 to denote that they are equivalent.
- *
- * @version 1.5 12/17/97
- * @author Philip Milne, Anthony Eden
- */
-public class TableSorter
-extends AbstractTableModel
-implements TableModelListener
+public class WbTableSorter 
+	extends AbstractTableModel 
+	implements TableModelListener
 {
 	protected TableModel model;
+	RowIndex[]   indexes;
+	Vector  sortingColumns = new Vector();
+	boolean ascending = true;
+	int     column = -1;
+	int     compares;
+	ColumnComparator comparator;
 	
-	public TableSorter(TableModel model)
+	public WbTableSorter(TableModel model)
 	{
 		setModel(model);
+		this.comparator = new ColumnComparator();
 	}
 	
 	public TableModel getModel()
@@ -112,7 +58,7 @@ implements TableModelListener
 	public Object getValueAt(int aRow, int aColumn)
 	{
 		checkModel();
-		return model.getValueAt(indexes[aRow], aColumn);
+		return model.getValueAt(indexes[aRow].rowIndex, aColumn);
 	}
 	
 	/**    Set the value at the given row and column.
@@ -124,7 +70,7 @@ implements TableModelListener
 	public void setValueAt(Object aValue, int aRow, int aColumn)
 	{
 		checkModel();
-		model.setValueAt(aValue, indexes[aRow], aColumn);
+		model.setValueAt(aValue, indexes[aRow].rowIndex, aColumn);
 	}
 	
 	/**    Get the row count.
@@ -221,6 +167,19 @@ implements TableModelListener
 		{
 			return 1;
 		}
+		
+		try
+		{
+			//System.out.println("Comparing " + o1 + " to " + o2);
+			int result = ((Comparable)o1).compareTo(o2);
+			//System.out.println("Result=" + result);
+			return result;
+			
+		}
+		catch (ClassCastException e)
+		{
+		}
+		
 		/*
 		 * We copy all returned values from the getValue call in case
 		 * an optimised model is reusing one object to return many
@@ -357,13 +316,10 @@ implements TableModelListener
 	public void reallocateIndexes()
 	{
 		int rowCount = model.getRowCount();
-		// Set up a new array of indexes with the right number of elements
-		// for the new data model.
-		indexes = new int[rowCount];
-		// Initialise with the identity mapping.
+		indexes = new RowIndex[rowCount];
 		for (int row = 0; row < rowCount; row++)
 		{
-			indexes[row] = row;
+			indexes[row] = new RowIndex(row);
 		}
 	}
 	
@@ -382,96 +338,18 @@ implements TableModelListener
 	 *
 	 * @param sender The object which invoked the sort
 	 */
-	public void sort(Object sender)
+	public synchronized void sort(Object sender)
 	{
 		checkModel();
 		compares = 0;
-		// n2sort();
-		// qsort(0, indexes.length-1);
-		shuttlesort((int[])indexes.clone(), indexes, 0, indexes.length);
-		//System.out.println("Compares: "+compares);
-	}
-	
-	public void n2sort()
-	{
-		for (int i = 0; i < getRowCount(); i++)
-		{
-			for (int j = i + 1; j < getRowCount(); j++)
-			{
-				if (compare(indexes[i], indexes[j]) == -1)
-				{
-					swap(i, j);
-				}
-			}
-		}
-	}
-	
-	// This is a home-grown implementation which we have not had time
-	// to research - it may perform poorly in some circumstances. It
-	// requires twice the space of an in-place algorithm and makes
-	// NlogN assigments shuttling the values between the two
-	// arrays. The number of compares appears to vary between N-1 and
-	// NlogN depending on the initial order but the main reason for
-	// using it here is that, unlike qsort, it is stable.
-	public void shuttlesort(int[] from, int[] to, int low, int high)
-	{
-		if (high - low < 2)
-		{
-			return;
-		}
-		int middle = (low + high) / 2;
-		shuttlesort(to, from, low, middle);
-		shuttlesort(to, from, middle, high);
-		int p = low;
-		int q = middle;
-		/* This is an optional short-cut; at each recursive call,
-		check to see if the elements in this subset are already
-		ordered.  If so, no further comparisons are needed; the
-		sub-array can just be copied.  The array must be copied rather
-		than assigned otherwise sister calls in the recursion might
-		get out of sinc.  When the number of elements is three they
-		are partitioned so that the first set, [low, mid), has one
-		element and and the second, [mid, high), has two. We skip the
-		optimisation when the number of elements is three or less as
-		the first compare in the normal merge will produce the same
-		sequence of steps. This optimisation seems to be worthwhile
-		for partially ordered lists but some analysis is needed to
-		find out how the performance drops to Nlog(N) as the initial
-		order diminishes - it may drop very quickly.  */
-		if (high - low >= 4 && compare(from[middle - 1], from[middle]) <= 0)
-		{
-			for (int i = low; i < high; i++)
-			{
-				to[i] = from[i];
-			}
-			return;
-		}
-		// A normal merge.
-		for (int i = low; i < high; i++)
-		{
-			if (q >= high || (p < middle && compare(from[p], from[q]) <= 0))
-			{
-				to[i] = from[p++];
-			}
-			else
-			{
-				to[i] = from[q++];
-			}
-		}
-	}
-	
-	public void swap(int i, int j)
-	{
-		int tmp = indexes[i];
-		indexes[i] = indexes[j];
-		indexes[j] = tmp;
+		Arrays.sort(indexes, this.comparator);
 	}
 	
 	/**    Sort by the given column in ascending order.
 	 *
 	 * @param column The column
 	 */
-	public void sortByColumn(int column)
+	public synchronized void sortByColumn(int column)
 	{
 		sortByColumn(column, true);
 	}
@@ -481,12 +359,15 @@ implements TableModelListener
 	 * @param column The column
 	 * @param ascending True to sort in ascending order
 	 */
-	public void sortByColumn(int column, boolean ascending)
+	public synchronized void sortByColumn(int aColumn, boolean ascending)
 	{
-		this.column = column;
 		this.ascending = ascending;
-		sortingColumns.removeAllElements();
-		sortingColumns.addElement(new Integer(column));
+		if (aColumn != this.column)
+		{
+			this.column = column;
+			sortingColumns.removeAllElements();
+			sortingColumns.addElement(new Integer(column));
+		}
 		sort(this);
 		fireTableChanged(new TableModelEvent(this));
 	}
@@ -507,7 +388,7 @@ implements TableModelListener
 	 */
 	public int getRealIndex(int row)
 	{
-		return indexes[row];
+		return indexes[row].rowIndex;
 	}
 	
 	/**    Return true if the data is sorted in ascending order.
@@ -531,24 +412,23 @@ implements TableModelListener
 	 *
 	 * @param table The JTable
 	 */
-	public void addMouseListenerToHeaderInTable(JTable table)
+	public void addMouseListenerToHeaderInTable(WbTable table)
 	{
-		final TableSorter sorter = this;
-		final JTable tableView = table;
+		final WbTableSorter sorter = this;
+		final WbTable tableView = table;
 		tableView.setColumnSelectionAllowed(false);
 		MouseAdapter listMouseListener = new MouseAdapter()
 		{
 			public void mouseClicked(MouseEvent e)
 			{
 				TableColumnModel columnModel = tableView.getColumnModel();
-				int viewColumn = columnModel.getColumnIndexAtX(e.getX());
-				int column = tableView.convertColumnIndexToModel(viewColumn);
+				final int viewColumn = columnModel.getColumnIndexAtX(e.getX());
+				final int column = tableView.convertColumnIndexToModel(viewColumn);
+				//WbSwingUtilities.showWaitCursor(tableView.getTableHeader());
+
 				if (e.getClickCount() == 1 && column != -1)
 				{
-					//System.out.println("Sorting ...");
-					//int shiftPressed = e.getModifiers()&InputEvent.SHIFT_MASK;
-					//boolean ascending = (shiftPressed == 0);
-					if (TableSorter.this.column == column)
+					if (WbTableSorter.this.column == column)
 					{
 						ascending = !ascending;
 					}
@@ -556,7 +436,21 @@ implements TableModelListener
 					{
 						ascending = true;
 					}
-					sorter.sortByColumn(column, ascending);
+					Thread sortThread = new Thread(new Runnable()
+							{
+								public void run()
+								{
+									tableView.sortingStarted();
+									tableView.getTableHeader().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+									tableView.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+									sorter.sortByColumn(column, ascending);
+									tableView.getTableHeader().setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+									tableView.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+									tableView.sortingFinished();
+									tableView.repaint();
+								}
+							});
+					sortThread.start();
 				}
 			}
 		};
@@ -564,9 +458,35 @@ implements TableModelListener
 		th.addMouseListener(listMouseListener);
 	}
 	
-	int[]   indexes;
-	Vector  sortingColumns = new Vector();
-	boolean ascending = true;
-	int     column = -1;
-	int     compares;
+	class RowIndex
+	{
+		public int rowIndex;
+		public RowIndex(int i)
+		{
+			this.rowIndex = i;
+		}
+	}
+	
+	class ColumnComparator
+		implements Comparator
+	{
+		public ColumnComparator()
+		{
+		}
+		
+		public int compare(Object o1, Object o2)
+		{
+			try
+			{
+				RowIndex row1 = (RowIndex)o1;
+				RowIndex row2 = (RowIndex)o2;
+				return WbTableSorter.this.compare(row1.rowIndex, row2.rowIndex);
+			}
+			catch (ClassCastException e)
+			{
+				// cannot happen
+			}
+			return 0;
+		}
+	}
 }
