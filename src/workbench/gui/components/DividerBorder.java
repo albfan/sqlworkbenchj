5e6ab85569b33a5d3a2d5aaad3dd3f73d03067c9
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2002-07-17 22:30:32 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@47 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/gui/components/DividerBorder.java
+++ src/workbench/gui/components/DividerBorder.java
@@ -18,23 +18,16 @@ public class DividerBorder extends AbstractBorder
 	protected int type;
 	protected int thickness;
 	
-	/**
-	 * Creates a line border with the specified color and a
-	 * thickness = 1.
-	 * @param color the color for the border
-	 */
 	public DividerBorder(int type)
 	{
 		this(type, 1);
 	}
 	
 	/**
-	 * Creates a line border with the specified color, thickness,
-	 * and corner shape.
-	 * @param color the color of the border
+	 * Creates a divider border with the specified type and thickness
+	 * @param type (LEFT, RIGHT, TOP, BOTTOM)
 	 * @param thickness the thickness of the border
 	 * @param roundedCorners whether or not border corners should be round
-	 * @since 1.3
 	 */
 	public DividerBorder(int aType, int aThickness)
 	{
@@ -42,16 +35,6 @@ public class DividerBorder extends AbstractBorder
 		this.type = aType;
 	}
 	
-	/**
-	 * Paints the border for the specified component with the
-	 * specified position and size.
-	 * @param c the component for which this border is being painted
-	 * @param g the paint graphics
-	 * @param x the x position of the painted border
-	 * @param y the y position of the painted border
-	 * @param width the width of the painted border
-	 * @param height the height of the painted border
-	 */
 	public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
 	{
 		Color oldColor = g.getColor();
@@ -94,20 +77,11 @@ public class DividerBorder extends AbstractBorder
 		g.setColor(oldColor);
 	}
 	
-	/**
-	 * Returns the insets of the border.
-	 * @param c the component for which this border insets value applies
-	 */
 	public Insets getBorderInsets(Component c)
 	{
 		return new Insets(2, 2, 2, 2);
 	}
 	
-	/**
-	 * Reinitialize the insets parameter with this Border's current Insets.
-	 * @param c the component for which this border insets value applies
-	 * @param insets the object to be reinitialized
-	 */
 	public Insets getBorderInsets(Component c, Insets insets)
 	{
 		insets.left = insets.top = insets.right = insets.bottom = 2;
---
+++ src/workbench/gui/components/UnderlineBorder.java
@@ -0,0 +1,48 @@
+package workbench.gui.components;
+
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import javax.swing.JLabel;
+import javax.swing.border.AbstractBorder;
+
+public class UnderlineBorder extends AbstractBorder
+{
+	protected JLabel label;
+	private Insets insets = new Insets(0, 0, 0, 0);
+	public UnderlineBorder(JLabel aLabel)
+	{
+		this.label = aLabel;
+	}
+	
+	public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
+	{
+		Color oldColor = g.getColor();
+		
+    Color fg = this.label.getForeground();
+		g.setColor(fg);
+		Font f = this.label.getFont();
+		FontMetrics fm = this.label.getFontMetrics(f);
+		int size = fm.stringWidth(this.label.getText());
+		g.drawLine(x, y + height - 1, x + size, y + height -1);
+		g.setColor(oldColor);
+	}
+	
+	public Insets getBorderInsets(Component c)
+	{
+		return insets;
+	}
+	
+	public Insets getBorderInsets(Component c, Insets insets)
+	{
+		insets.left = insets.top = insets.right = insets.bottom = 0;
+		return insets;
+	}
+	
+
+}
+
