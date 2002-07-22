4a59a586b30367a8cf17ebb98c0b522b52bdde15
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2002-07-22 19:25:55 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@49 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/actions/SaveSqlHistoryAction.java
@@ -0,0 +1,39 @@
+package workbench.gui.actions;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+import workbench.gui.sql.SqlPanel;
+import workbench.interfaces.TextContainer;
+import workbench.resource.ResourceMgr;
+import workbench.util.StringUtil;
+
+/**
+ *	Action to clear the contents of a entry field
+ *	@author  sql.workbench@freenet.de
+ */
+public class SaveSqlHistoryAction extends WbAction
+{
+	private SqlPanel client;
+
+	public SaveSqlHistoryAction(SqlPanel aClient)
+	{
+		super();
+		this.client = aClient;
+		this.putValue(Action.NAME, ResourceMgr.getString("MnuTxtSaveSqlHistory"));
+		this.putValue(WbAction.MAIN_MENU_ITEM, ResourceMgr.MNU_TXT_SQL);
+		this.putValue(Action.SHORT_DESCRIPTION, ResourceMgr.getDescription("MnuTxtCreateSnippet"));
+		//this.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.ALT_MASK));
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		this.client.saveSqlStatementHistory();
+	}
+}
