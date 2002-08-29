25d7bec1724f9647ece516cff1f196899b7757b3
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2002-08-29 17:45:46 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@87 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/components/PasswordPropertyEditor.java
@@ -0,0 +1,103 @@
+/*
+ * Created on 28. August 2002, 23:40
+ */
+package workbench.gui.components;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import workbench.interfaces.SimplePropertyEditor;
+
+
+/**
+ *
+ * @author  workbench@kellerer.org
+ */
+public class PasswordPropertyEditor 
+	extends JPasswordField implements DocumentListener, SimplePropertyEditor
+{
+	private Object source;
+	private Method setter;
+	private Method getter;
+	private boolean changed;
+	private String propName;
+	
+	public PasswordPropertyEditor()
+	{
+		super();
+	}
+	
+	public void setSourceObject(Object aSource, String aProperty)
+	{
+		this.source = aSource;
+		this.changed = false;
+		this.propName = aProperty;
+		String propertyName = Character.toUpperCase(aProperty.charAt(0)) + aProperty.substring(1);
+		
+		this.getDocument().removeDocumentListener(this);
+		
+		try
+		{
+			String name = "decryptPassword";
+			Class cls = aSource.getClass();
+			this.getter = cls.getMethod(name, null);
+
+			name = "setPassword";
+			Class[] parms = {String.class};
+			this.setter = cls.getMethod(name, parms);
+
+			String value = (String)this.getter.invoke(this.source, null);
+			this.setText(value);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Error on init");
+			e.printStackTrace();
+		}
+		this.getDocument().addDocumentListener(this);
+	}
+	
+	public void applyChanges()
+	{
+		if (!this.changed) return;
+		if (this.source == null) return;
+		if (this.setter == null) return;
+		Object args[] = new Object[1];
+		args[0] = this.getText();
+		try
+		{
+			this.setter.invoke(this.source, args);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean isChanged() { return this.changed; }
+	
+	public void changedUpdate(DocumentEvent e)
+	{
+		this.changed = true;
+		super.firePropertyChange(this.propName, null, null);
+	}
+	
+	public void insertUpdate(DocumentEvent e)
+	{
+		this.changed = true;
+		super.firePropertyChange(this.propName, null, null);
+	}
+	
+	public void removeUpdate(DocumentEvent e)
+	{
+		this.changed = true;
+		super.firePropertyChange(this.propName, null, null);
+	}
+	
+}
+
