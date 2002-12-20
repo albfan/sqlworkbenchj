b07572efe31812bddf82f323263a1695323b5326
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2002-12-20 23:36:50 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@122 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/sql/wbcommands/WbListCatalogs.java
@@ -0,0 +1,38 @@
+package workbench.sql.wbcommands;
+
+import java.sql.SQLException;
+import workbench.db.WbConnection;
+import workbench.exception.WbException;
+import workbench.sql.SqlCommand;
+import workbench.sql.StatementRunnerResult;
+import workbench.storage.DataStore;
+
+/**
+ *
+ * @author  workbench@kellerer.org
+ */
+public class WbListCatalogs extends SqlCommand
+{
+	public final String VERB;
+	
+	public static final WbListCatalogs LISTDB = new WbListCatalogs("LISTDB");
+	public static final WbListCatalogs LISTCAT = new WbListCatalogs("LISTCAT");
+	
+	private WbListCatalogs(String verb)
+	{
+		this.VERB = verb;
+	}
+	
+	public String getVerb() { return VERB; }
+	
+	public StatementRunnerResult execute(WbConnection aConnection, String aSql) 
+		throws SQLException, WbException
+	{
+		StatementRunnerResult result = new StatementRunnerResult(aSql);
+		DataStore ds = aConnection.getMetadata().getCatalogInformation();
+		result.addDataStore(ds);
+		result.setSuccess();
+		return result;
+	}	
+	
+}
