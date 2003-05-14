2d3c8a0fd9f8e61655d09147cd5fb060531fd7a5
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2003-05-14 18:02:08 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@144 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/storage/RowActionMonitor.java
@@ -0,0 +1,19 @@
+/*
+ * DataStoreUpdateMonitor.java
+ *
+ * Created on May 8, 2003, 12:11 PM
+ */
+
+package workbench.storage;
+
+/**
+ *
+ * @author  tkellerer
+ */
+public interface RowActionMonitor
+{
+	final int MONITOR_INSERT = 0;
+	final int MONITOR_UPDATE = 1;
+	void setMonitorType(int aType);
+	void setCurrentRow(int currentRow, int totalRows);
+}
