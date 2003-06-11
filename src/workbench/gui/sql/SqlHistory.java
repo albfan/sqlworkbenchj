f64f1584de9175f050a9f412f9a913807d64e05a
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2003-06-11 16:55:37 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@158 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/sql/SqlHistory.java
@@ -0,0 +1,203 @@
+/*
+ * SqlHistory.java
+ *
+ * Created on June 4, 2003, 6:26 PM
+ */
+
+package workbench.gui.sql;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import workbench.log.LogMgr;
+import workbench.util.StringUtil;
+
+/**
+ *
+ * @author  thomas
+ */
+public class SqlHistory
+{
+	private ArrayList history;
+	private int currentEntry;
+	private int maxSize;
+
+	public SqlHistory(int maxSize)
+	{
+		this.maxSize = maxSize;
+		this.history = new ArrayList(maxSize + 2);
+	}
+
+	public void addContent(EditorPanel editor)
+	{
+		SqlHistoryEntry entry = new SqlHistoryEntry(editor.getText(), editor.getCaretPosition(), editor.getSelectionStart(), editor.getSelectionEnd());
+		SqlHistoryEntry top = this.getTopEntry();
+		if (top != null && top.equals(entry)) return;
+		this.addEntry(entry);
+	}
+
+	public void addEntry(SqlHistoryEntry entry)
+	{
+		this.history.add(entry);
+		if (this.history.size() > this.maxSize)
+		{
+			this.history.remove(0);
+		}
+		this.currentEntry = this.history.size() - 1;
+	}
+
+	public boolean hasNext()
+	{
+		return (this.currentEntry < (this.history.size() - 1));
+	}
+
+	public boolean hasPrevious()
+	{
+		return (this.currentEntry > 0);
+	}
+
+	public void clear()
+	{
+		this.currentEntry = 0;
+		this.history.clear();
+	}
+
+	public void showCurrent(EditorPanel editor)
+	{
+		if (this.currentEntry >= this.history.size()) return;
+		SqlHistoryEntry entry = (SqlHistoryEntry)this.history.get(this.currentEntry);
+		entry.applyTo(editor);
+	}
+
+	public void showPrevious(EditorPanel editor)
+	{
+		if (!this.hasPrevious()) return;
+		SqlHistoryEntry entry = this.getPreviousEntry();
+		entry.applyTo(editor);
+	}
+
+	public void showNext(EditorPanel editor)
+	{
+		if (!this.hasNext()) return;
+		SqlHistoryEntry entry = this.getNextEntry();
+		entry.applyTo(editor);
+	}
+
+	public SqlHistoryEntry getTopEntry()
+	{
+		if (this.history.size() < 1) return null;
+		SqlHistoryEntry entry = (SqlHistoryEntry)this.history.get(this.history.size() - 1);
+		return entry;
+	}
+
+	private SqlHistoryEntry getPreviousEntry()
+	{
+		if (this.currentEntry <= 0) return null;
+		this.currentEntry--;
+		SqlHistoryEntry entry = (SqlHistoryEntry)this.history.get(this.currentEntry);
+		return entry;
+	}
+
+	private SqlHistoryEntry getNextEntry()
+	{
+		if (this.currentEntry >= this.history.size() - 1) return null;
+		this.currentEntry++;
+		SqlHistoryEntry entry = (SqlHistoryEntry)this.history.get(this.currentEntry);
+		return entry;
+	}
+
+	private static final String KEY_POS = "##sqlwb.pos=";
+	private static final String KEY_START = "##sqlwb.selStart=";
+	private static final String KEY_END = "##sqlwb.selEnd=";
+
+	public void writeToStream(OutputStream out)
+	{
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+		try
+		{
+			int count = this.history.size();
+			for (int i=0; i < count; i++)
+			{
+				SqlHistoryEntry entry = (SqlHistoryEntry)this.history.get(i);
+				writer.write(KEY_POS);
+				writer.write(Integer.toString(entry.getCursorPosition()));
+				writer.write(StringUtil.LINE_TERMINATOR);
+
+				writer.write(KEY_START);
+				writer.write(Integer.toString(entry.getSelectionStart()));
+				writer.write(StringUtil.LINE_TERMINATOR);
+
+				writer.write(KEY_END);
+				writer.write(Integer.toString(entry.getSelectionEnd()));
+				writer.write(StringUtil.LINE_TERMINATOR);
+
+				writer.write(entry.getText());
+				writer.write(StringUtil.LINE_TERMINATOR);
+				writer.write(StringUtil.LIST_DELIMITER);
+				writer.write(StringUtil.LINE_TERMINATOR);
+			}
+			writer.flush();
+		}
+		catch (IOException e)
+		{
+			LogMgr.logError("SqlHistory.writeToStream()", "Could not write history!", e);
+		}
+	}
+
+	public void readFromStream(InputStream in)
+	{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		StringBuffer content = new StringBuffer(500);
+		int pos = 0;
+		int start = -1;
+		int end = -1;
+		try
+		{
+			String line = reader.readLine();
+			while(line != null)
+			{
+				if (line.equals(StringUtil.LIST_DELIMITER))
+				{
+					SqlHistoryEntry entry = new SqlHistoryEntry(content.toString(), pos, start, end);
+					this.addEntry(entry);
+					pos = 0;
+					start = -1;
+					end = -1;
+					content = new StringBuffer(500);
+				}
+				else if (line.startsWith(KEY_POS))
+				{
+					pos = StringUtil.getIntValue(line.substring(KEY_POS.length()), -1);
+				}
+				else if (line.startsWith(KEY_START))
+				{
+					start = StringUtil.getIntValue(line.substring(KEY_START.length()), -1);
+				}
+				else if (line.startsWith("##sqlwb.selEnd="))
+				{
+					end = StringUtil.getIntValue(line.substring(KEY_END.length()), -1);
+				}
+				else
+				{
+					content.append(line.trim());
+					content.append('\n');
+				}
+				line = reader.readLine();
+			}
+		}
+		catch (IOException e)
+		{
+			LogMgr.logError("SqlHistory.readFromStream()", "Could not read history!", e);
+		}
+		if (content.length() > 0)
+		{
+			SqlHistoryEntry entry = new SqlHistoryEntry(content.toString(), pos, start, end);
+			this.addEntry(entry);
+		}
+	}
+}
---
+++ src/workbench/gui/sql/SqlHistoryEntry.java
@@ -0,0 +1,108 @@
+/*
+ * SqlHistoryEntry.java
+ *
+ * Created on June 4, 2003, 6:27 PM
+ */
+
+package workbench.gui.sql;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author  thomas
+ */
+public class SqlHistoryEntry
+{
+
+	private String text;
+	private int cursorPos;
+	private int selectionStart;
+	private int selectionEnd;
+	
+	private static final Pattern PATTERN_EMPTY_LINE = Pattern.compile("$(\r\n|\n\r|\r|\n)");
+
+	public SqlHistoryEntry(String sql, int pos, int selStart, int selEnd)
+	{
+		this.text = this.trimEmptyLines(sql);
+		int len = this.text.length();
+		if (pos >= len) 
+			this.cursorPos = len - 1;
+		else
+			this.cursorPos = pos;
+		this.selectionStart = selStart;
+		this.selectionEnd = selEnd;
+	}
+
+	public SqlHistoryEntry(String sql)
+	{
+		this.text = this.trimEmptyLines(sql);
+		this.cursorPos = -1;
+		this.selectionStart = -1;
+		this.selectionEnd = -1;
+	}
+
+	public String toString() { return this.text; }
+	public String getText() { return this.text; }
+	public int getCursorPosition() { return this.cursorPos; }
+	public int getSelectionStart() { return this.selectionStart; }
+	public int getSelectionEnd() { return this.selectionEnd; }
+
+	public void applyTo(EditorPanel editor)
+	{
+		if (editor == null) return;
+		editor.setText(this.text);
+		if (this.cursorPos > -1) editor.setCaretPosition(this.cursorPos);
+		if (this.selectionStart > -1 && this.selectionEnd > this.selectionStart)
+		{
+			editor.setSelectionStart(this.selectionStart);
+			editor.setSelectionEnd(this.selectionEnd);
+		}
+	}
+
+	public boolean equals(Object o)
+	{
+		if (!(o instanceof SqlHistoryEntry)) return false;
+		SqlHistoryEntry other = (SqlHistoryEntry)o;
+		if (this.text.equals(other.text))
+		{
+			return (this.cursorPos == other.cursorPos &&
+				      this.selectionEnd == other.selectionEnd &&
+						  this.selectionStart == other.selectionStart);
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	private static String trimEmptyLines(String input)
+	{
+		if (input == null) return null;
+		int len = input.length() - 1;
+		
+		char c = input.charAt(len);
+		while ( (c == '\r' || c == '\n') && len > 0)
+		{
+			len --;
+			c = input.charAt(len);
+		}
+		return input.substring(0, len + 1);
+	}
+	
+	public static void main(String args[])
+	{
+		try
+		{
+			String test = "test\r\ntest2\r\n\r\n";
+			System.out.println(">" + trimEmptyLines(test) + "<");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		System.out.println("done.");
+		System.exit(0);
+	}
+}
