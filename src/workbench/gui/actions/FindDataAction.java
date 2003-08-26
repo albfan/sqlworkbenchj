3e26a5e638d88cab401af7eb71f019b502e9f83e
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2003-08-26 16:43:05 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@176 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/actions/FindDataAction.java
@@ -0,0 +1,37 @@
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+
+import workbench.interfaces.Searchable;
+import workbench.resource.ResourceMgr;
+
+/**
+ *	Action to clear the contents of a entry field
+ *	@author  workbench@kellerer.org
+ */
+public class FindDataAction extends WbAction
+{
+	private Searchable client;
+
+	public FindDataAction(Searchable aClient)
+	{
+		super();
+		this.client = aClient;
+		this.putValue(Action.NAME, ResourceMgr.getString("MnuTxtFindData"));
+		//this.putValue(Action.SMALL_ICON, ResourceMgr.getImage(ResourceMgr.IMG_FIND));
+		this.putValue(WbAction.MAIN_MENU_ITEM, ResourceMgr.MNU_TXT_DATA);
+		this.putValue(Action.SHORT_DESCRIPTION, ResourceMgr.getDescription("MnuTxtFindData"));
+		this.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
+		this.setCreateToolbarSeparator(true);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		this.client.find();
+	}
+}
---
+++ src/workbench/gui/actions/FindDataAgainAction.java
@@ -0,0 +1,42 @@
+/*
+ * ClearAction.java
+ *
+ * Created on December 2, 2001, 1:32 AM
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+
+import workbench.interfaces.Searchable;
+import workbench.resource.ResourceMgr;
+
+
+/**
+ *	Action to clear the contents of a entry field
+ *	@author  workbench@kellerer.org
+ */
+public class FindDataAgainAction extends WbAction
+{
+	private Searchable client;
+
+	public FindDataAgainAction(Searchable aClient)
+	{
+		super();
+		this.client = aClient;
+		this.putValue(Action.NAME, ResourceMgr.getString("MnuTxtFindDataAgain"));
+		//this.putValue(Action.SMALL_ICON, ResourceMgr.getImage("FindAgain"));
+		this.putValue(WbAction.MAIN_MENU_ITEM, ResourceMgr.MNU_TXT_DATA);
+		this.putValue(Action.SHORT_DESCRIPTION, ResourceMgr.getDescription("MnuTxtFindDataAgain"));
+		this.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F3, InputEvent.SHIFT_MASK));
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		this.client.findNext();
+	}
+}
