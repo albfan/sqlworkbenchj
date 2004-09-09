3bc46d995768b73bec9afda60c7ca09dfe41df61
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2004-09-09 22:12:16 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@283 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/db/report/ColumnReference.java
@@ -0,0 +1,62 @@
+/*
+ * ColumnReference.java
+ *
+ * Created on September 9, 2004, 11:37 PM
+ */
+
+package workbench.db.report;
+
+import workbench.util.StrBuffer;
+
+/**
+ *
+ * @author  workbench@kellerer.org
+ */
+public class ColumnReference
+{
+	public static final String TAG_REFERENCE = "references";
+	public static final String TAG_CONSTRAINT_NAME = "constraint-name";
+	public static final String TAG_UPDATE_RULE = "update-rule";
+	public static final String TAG_DELETE_RULE = "delete-rule";
+	
+	private String fkName;
+	private String foreignColumn;
+	private String foreignTable;
+	private String updateRule;
+	private String deleteRule;
+	private TagWriter tagWriter = new TagWriter();
+	
+	public ColumnReference()
+	{
+	}
+	
+	public void setNamespace(String namespace)
+	{
+		this.tagWriter.setNamespace(namespace);
+	}
+	public void setConstraintName(String name) { this.fkName = name; }
+	public void setForeignColumn(String col) { this.foreignColumn = col; }
+	public void setForeignTable(String tbl) { this.foreignTable = tbl; }
+	public void setUpdateRule(String rule) { this.updateRule = rule; }
+	public void setDeleteRule(String rule) { this.deleteRule = rule; }
+	
+	public StrBuffer getXml(StrBuffer indent)
+	{
+		StrBuffer result = new StrBuffer(250);
+		StrBuffer myindent = new StrBuffer(indent);
+		myindent.append("  ");
+		tagWriter.appendOpenTag(result, indent, TAG_REFERENCE);
+		result.append('\n');
+		
+		tagWriter.appendTag(result, myindent, ReportTable.TAG_TABLE_NAME, this.foreignTable);
+		tagWriter.appendTag(result, myindent, ReportColumn.TAG_COLUMN_NAME, this.foreignColumn);
+		tagWriter.appendTag(result, myindent, TAG_CONSTRAINT_NAME, this.fkName);
+		tagWriter.appendTag(result, myindent, TAG_DELETE_RULE, this.deleteRule);
+		tagWriter.appendTag(result, myindent, TAG_UPDATE_RULE, this.updateRule);
+		
+		tagWriter.appendCloseTag(result, indent, TAG_REFERENCE);
+		
+		return result;
+	}
+	
+}
