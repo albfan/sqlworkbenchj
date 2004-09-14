f44cc9ddcbb09eaa659bb7dd1ecf043cc1f1a40b
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2004-09-14 19:38:16 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@293 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/storage/RowDataList.java
@@ -0,0 +1,117 @@
+package workbench.storage;
+
+import java.util.Comparator;
+import java.util.Arrays;
+
+public class RowDataList
+{
+	public static final RowDataList EMPTY_BUFFER = new RowDataList();
+	private static final int DEFAULT_SIZE = 150;
+
+	private int size;
+	private RowData data[];
+
+	public RowDataList()
+	{
+		this(DEFAULT_SIZE);
+	}
+
+	public RowDataList(int len)
+	{
+		this.data = new RowData[len];
+		this.size = 0;
+	}
+
+	private void grow(int minStorage)
+	{
+		int newStorage = (int)(this.data.length * 1.5) + 1;
+
+		if (newStorage < minStorage) newStorage = minStorage;
+		
+		if (this.data != null)
+		{
+			RowData newBuf[] = new RowData[newStorage];
+			System.arraycopy(this.data, 0, newBuf, 0, this.size);
+			this.data = newBuf;
+		}
+	}
+
+	/**
+	 * 	Free all objects stored in the internal array.
+	 * 	This does not shrink the array itself.
+	 */
+	public void clear()
+	{
+		for (int i=0; i < size; i++)
+		{
+			data[i].reset();
+			data[i] = null;
+		}
+		this.size = 0;
+	}
+
+	/**
+	 * 	Free all objects stored in the internal array.
+	 * 	and release
+	 */
+	public void reset()
+	{
+		this.clear();
+		this.data = new RowData[DEFAULT_SIZE];
+	}
+
+	public int size()
+	{
+		return this.size;
+	}
+
+
+	public RowData get(int index)
+	{
+		return this.data[index];
+	}
+
+	public void remove(int index)
+	{
+		int count = size - index - 1;
+
+		if (count > 0)
+		{
+			System.arraycopy(data, index+1, data, index, count);
+		}
+		this.size --;
+		this.data[size].reset();
+		this.data[size] = null;
+	}
+
+	public int add(RowData row)
+	{
+		int newlen = this.size + 1;
+		if (newlen > this.data.length) grow(newlen);
+		this.data[newlen - 1] = row;
+		this.size = newlen;
+		return this.size;
+	}
+
+	public int add(int index, RowData row)
+	{
+		int newlen = this.size + 1;
+		if (newlen > this.data.length)
+		{
+			// we are not using moreStorage here to optimize
+			// the calls to System.arraycopy
+			RowData newBuf[] = new RowData[newlen + 10];
+			System.arraycopy(this.data, 0, newBuf, 0, index);
+			System.arraycopy(this.data, index, newBuf, index + 1, (size - index));
+			this.data = newBuf;
+		}
+		this.data[index] = row;
+		this.size = newlen;
+		return index;
+	}
+
+	public void sort(Comparator comp)
+	{
+		Arrays.sort(this.data, 0, this.size, comp);
+	}
+}
---
+++ src/workbench/util/WbThread.java
@@ -0,0 +1,25 @@
+/*
+ * WbThread.java
+ *
+ * Created on 10. September 2004, 15:25
+ */
+
+package workbench.util;
+
+/**
+ *
+ * @author tkellerer
+ */
+public class WbThread
+	extends Thread
+{
+	
+	/** Creates a new instance of WbThread */
+	public WbThread(String name)
+	{
+		super();
+		this.setName(name);
+		this.setDaemon(true);
+	}
+	
+}
