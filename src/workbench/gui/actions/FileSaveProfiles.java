6b9ce157f624b49f921d68a95c22e004337b6037
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2004-11-15 20:04:38 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@302 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/actions/FileSaveProfiles.java
@@ -0,0 +1,44 @@
+/*
+ * FileSaveProfiles.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+
+import workbench.WbManager;
+import workbench.db.ConnectionMgr;
+import workbench.exception.ExceptionUtil;
+import workbench.gui.WbSwingUtilities;
+import workbench.log.LogMgr;
+import workbench.resource.ResourceMgr;
+
+public class FileSaveProfiles extends WbAction
+{
+	public FileSaveProfiles()
+	{
+		super();
+		this.initMenuDefinition("MnuTxtFilesSaveProfiles");
+	}
+
+	public void executeAction(ActionEvent e)
+	{
+		try
+		{
+			ConnectionMgr.getInstance().saveProfiles();
+			WbSwingUtilities.showMessage(WbManager.getInstance().getCurrentWindow(), ResourceMgr.getString("MsgProfilesSaved"));
+		}
+		catch (Exception ex)
+		{
+			LogMgr.logError("FileSaveProfiles.executeAction()", "Error saving profiles", ex);
+			WbSwingUtilities.showMessage(WbManager.getInstance().getCurrentWindow(), ResourceMgr.getString("ErrorSavingProfiles") + "\n" + ExceptionUtil.getDisplay(ex));
+		}
+	}
+}
