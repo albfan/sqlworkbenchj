3f106a178d02c79ec38c863465e33e38b59e78e0
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2004-11-18 22:12:41 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@307 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/util/CharacterRange.java
@@ -0,0 +1,109 @@
+/*
+ * CharacterRange.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.util;
+
+import workbench.resource.ResourceMgr;
+
+/**
+ * @author  info@sql-workbench.net
+ */
+public class CharacterRange
+{
+	public static final CharacterRange RANGE_NONE = new CharacterRange(0);
+	public static final CharacterRange RANGE_CONTROL = new CharacterRange(1);
+	public static final CharacterRange RANGE_7BIT = new CharacterRange(2);
+	public static final CharacterRange RANGE_8BIT = new CharacterRange(3);
+	public static final CharacterRange RANGE_8BIT_EXTENDED = new CharacterRange(4);
+	
+	private static final int MAX_ID = 4;
+	
+	private int typeIndex = 0;
+	
+	private CharacterRange(int index)
+	{
+		typeIndex = index;
+	}
+	
+	public static boolean isValidId(int id)
+	{
+		return id >= 0 && id <= 4;
+	}
+	
+	public static CharacterRange[] getRanges()
+	{
+		return new CharacterRange[] { RANGE_NONE, RANGE_CONTROL, RANGE_7BIT, RANGE_8BIT, RANGE_8BIT_EXTENDED };
+	}
+	
+	public static CharacterRange getRangeById(int index)
+	{
+		switch (index)
+		{
+			case 0:
+				return RANGE_NONE;
+			case 1:
+				return RANGE_CONTROL;
+			case 2:
+				return RANGE_7BIT;
+			case 3:
+				return RANGE_8BIT;
+			case 4: 
+				return RANGE_8BIT_EXTENDED;
+		}
+		return null;
+	}
+	
+	public int getId()
+	{
+		return typeIndex;
+	}
+	
+	public boolean isOutsideRange(char c)
+	{
+		switch (typeIndex)
+		{
+			case 0:
+				return false;
+			case 1:
+				return (c < 32);
+			case 2:
+				return (c < 32 || c > 126);
+			case 3:
+				return (c < 32 || c > 255);
+			case 4: 
+				return (c < 32 || (c > 126 && c < 161) || c > 255);
+		}
+		return false;
+	}
+
+	public String getFilterDescription()
+	{
+		switch (typeIndex)
+		{
+			case 0:
+				return ResourceMgr.getString("LabelExportRangeNone");
+			case 1:
+				return ResourceMgr.getString("LabelExportRangeCtrl");
+			case 2:
+				return ResourceMgr.getString("LabelExportRange7Bit");
+			case 3:
+				return ResourceMgr.getString("LabelExportRange8Bit");
+			case 4: 
+				return ResourceMgr.getString("LabelExportRange8BitExtended");
+		}
+		return "";
+	}
+	
+	public String toString()
+	{
+		return getFilterDescription();
+	}
+}
