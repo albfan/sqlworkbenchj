6ce558be1fb0583d85d383b7d4eedf19d6da3198
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2004-11-21 15:00:26 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@310 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/db/SequenceReader.java
@@ -0,0 +1,34 @@
+/*
+ * SequenceReader.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ */
+package workbench.db;
+
+import java.util.List;
+import workbench.storage.DataStore;
+
+/**
+ * @author info@sql-workbench.net
+ */
+public interface SequenceReader
+{
+	/**
+	 *	Return a SQL String to recreate the given sequence
+	 */
+	String getSequenceSource(String owner, String sequence);
+
+	/**
+	 * 	Get a list of sequences for the given owner. The 
+	 *  contains objects of type String.
+	 */
+	public List getSequenceList(String owner);
+	
+	public DataStore getSequenceDefinition(String owner, String sequence);
+	
+}
---
+++ src/workbench/util/LineEnding.java
@@ -0,0 +1,55 @@
+/*
+ * LineEnding.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.util;
+
+import workbench.resource.ResourceMgr;
+
+/**
+ * @author  info@sql-workbench.net
+ */
+public class LineEnding
+{
+	public static final LineEnding UNIX = new LineEnding("\n");
+	public static final LineEnding WIN = new LineEnding("\r\n");
+	public static final LineEnding DEFAULT = new LineEnding(null);
+	
+	private final String lineEnd;
+	private boolean isDefault = false;
+	private LineEnding(String ending)
+	{
+		isDefault = (ending == null);
+		if (isDefault)
+		{
+			lineEnd = StringUtil.LINE_TERMINATOR;
+		}
+		else
+		{
+			lineEnd = ending;
+		}
+	}
+	
+	public String toString()
+	{
+		if (isDefault)
+			return ResourceMgr.getString("LabelPlatformDefaultLineEnding");
+		if ("\n".equals(lineEnd)) 
+			return ResourceMgr.getString("LabelUnixLineEnding");
+		else 
+			return ResourceMgr.getString("LabelWindLineEnding");
+	}
+	
+	public String getLineEnding()
+	{
+		return lineEnd;
+	}
+	
+}
