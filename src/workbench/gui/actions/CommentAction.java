5d1b68262ccf0f7cd1a9f750a51426995ba1af41
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-01-19 17:37:12 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@321 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/actions/CommentAction.java
@@ -0,0 +1,43 @@
+/*
+ * CommentAction.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.KeyStroke;
+import workbench.gui.sql.EditorPanel;
+
+import workbench.interfaces.Undoable;
+import workbench.resource.ResourceMgr;
+
+/**
+ *	@author  info@sql-workbench.net
+ */
+public class CommentAction extends WbAction
+{
+	private EditorPanel client;
+
+	public CommentAction(EditorPanel aClient)
+	{
+		super();
+		this.client = aClient;
+		this.initMenuDefinition("MnuTxtCommentSelection",KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_MASK + InputEvent.SHIFT_MASK));
+		this.setMenuItemName(ResourceMgr.MNU_TXT_EDIT);
+	}
+
+	public void executeAction(ActionEvent e)
+	{
+		this.client.commentSelection();
+	}
+}
---
+++ src/workbench/gui/actions/UnCommentAction.java
@@ -0,0 +1,43 @@
+/*
+ * UnCommentAction.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.KeyStroke;
+import workbench.gui.sql.EditorPanel;
+
+import workbench.interfaces.Undoable;
+import workbench.resource.ResourceMgr;
+
+/**
+ *	@author  info@sql-workbench.net
+ */
+public class UnCommentAction extends WbAction
+{
+	private EditorPanel client;
+
+	public UnCommentAction(EditorPanel aClient)
+	{
+		super();
+		this.client = aClient;
+		this.initMenuDefinition("MnuTxtUnCommentSelection",KeyStroke.getKeyStroke(KeyEvent.VK_U, InputEvent.CTRL_MASK + InputEvent.SHIFT_MASK));
+		this.setMenuItemName(ResourceMgr.MNU_TXT_EDIT);
+	}
+
+	public void executeAction(ActionEvent e)
+	{
+		this.client.unCommentSelection();
+	}
+}
