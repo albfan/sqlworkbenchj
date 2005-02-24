5df8f78621be51811ce121d187b8131533707ce3
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-02-24 21:05:22 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@330 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/preparedstatement/ParameterEditor.java
@@ -0,0 +1,160 @@
+/*
+ * PSParameterEditor.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.gui.preparedstatement;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.sql.Types;
+import java.util.Properties;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableColumn;
+
+import workbench.WbManager;
+import workbench.gui.WbSwingUtilities;
+import workbench.gui.actions.DeleteListEntryAction;
+import workbench.gui.actions.NewListEntryAction;
+import workbench.gui.components.DataStoreTableModel;
+import workbench.gui.components.ValidatingDialog;
+import workbench.gui.components.WbTable;
+import workbench.gui.components.WbTextCellEditor;
+import workbench.interfaces.FileActions;
+import workbench.interfaces.ValidatingComponent;
+import workbench.log.LogMgr;
+import workbench.resource.ResourceMgr;
+import workbench.sql.VariablePool;
+import workbench.sql.preparedstatement.StatementParameters;
+import workbench.storage.DataStore;
+import workbench.gui.sql.*;
+import workbench.util.SqlUtil;
+
+/**
+ *
+ * @author  info@sql-workbench.net
+ *
+ * An component to enter parameters for prepared statements
+ */
+public class ParameterEditor 
+	extends JPanel 
+	implements ValidatingComponent
+{
+	private WbTable parameterTable;
+	private StatementParameters parameters;
+	private StatementParameterTableModel model;
+	
+	public ParameterEditor(StatementParameters parms)
+	{
+		this.parameters = parms;
+		this.model = new StatementParameterTableModel(this.parameters);
+		this.parameterTable = new WbTable();
+		this.parameterTable.setRowSelectionAllowed(false);
+		this.parameterTable.setColumnSelectionAllowed(false);
+		this.parameterTable.setModel(this.model);
+		this.parameterTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		TableColumn col = this.parameterTable.getColumnModel().getColumn(2);
+		col.setCellEditor(WbTextCellEditor.createInstance(true));
+
+		JLabel l = new JLabel(ResourceMgr.getString("TxtPSParameterInputText"));
+		Border b = BorderFactory.createEmptyBorder(5, 2, 5, 2);
+		l.setBorder(b);
+		l.setBackground(Color.WHITE);
+		l.setOpaque(true);
+		l.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		this.setLayout(new BorderLayout());
+
+		JScrollPane scroll = new JScrollPane(this.parameterTable);
+		b = BorderFactory.createEmptyBorder(5, 0, 0, 0);
+		Border b2 = BorderFactory.createCompoundBorder(b, scroll.getBorder());
+		scroll.setBorder(b2);
+		
+		this.add(l, BorderLayout.NORTH);
+		this.add(scroll, BorderLayout.CENTER);
+	}
+
+	public void applyValues()
+	{
+		this.parameterTable.stopEditing();
+		int count = this.parameters.getParameterCount();
+		for (int i=0; i < count; i++)
+		{
+			String v = this.model.getParameterValue(i);
+			this.parameters.setParameterValue(i, v);
+		}
+	}
+	
+	public void componentDisplayed()
+	{
+		this.parameterTable.setColumnSelectionInterval(2,2);
+		this.parameterTable.editCellAt(0, 2);
+		TableCellEditor editor = this.parameterTable.getCellEditor();
+		if (editor instanceof WbTextCellEditor)
+		{
+			WbTextCellEditor wbedit = (WbTextCellEditor)editor;
+			wbedit.selectAll();
+			wbedit.requestFocus();
+		}
+	}
+	
+	public boolean validateInput()
+	{
+		this.parameterTable.stopEditing();
+		int count = this.parameters.getParameterCount();
+		for (int i=0; i < count; i++)
+		{
+			String v = this.model.getParameterValue(i);
+			if (!this.parameters.isValueValid(i, v)) 
+			{
+				String error = ResourceMgr.getString("ErrorInvalidPSParameterValue").replaceAll("%value%", v);
+				error = error.replaceAll("%type%", SqlUtil.getTypeName(this.parameters.getParameterType(i)));
+				WbSwingUtilities.showErrorMessage(this, error);
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean showParameterDialog(StatementParameters parms)
+	{
+		ParameterEditor editor = new ParameterEditor(parms);
+		Dimension d = new Dimension(300,250);
+		editor.setMinimumSize(d);
+		editor.setPreferredSize(d);
+
+		boolean result = false; 
+		//boolean ok = ValidatingDialog.showConfirmDialog(WbManager.getInstance().getCurrentWindow(), editor, ResourceMgr.getString("TxtEditPSParameterWindowTitle"));
+		boolean ok = ValidatingDialog.showConfirmDialog(null, editor, ResourceMgr.getString("TxtEditPSParameterWindowTitle"));
+		if (ok)
+		{
+			try
+			{
+				editor.applyValues();
+				result = true;
+			}
+			catch (Exception e)
+			{
+				LogMgr.logError("VariablesEditor.showVariablesDialog()", "Error when saving values", e);
+				result = false;
+			}
+		}
+		return result;
+	}
+	
+}
---
+++ src/workbench/gui/preparedstatement/StatementParameterTableModel.java
@@ -0,0 +1,116 @@
+/*
+ * StatementParameterDataModel.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.gui.preparedstatement;
+
+import javax.swing.table.TableModel;
+import workbench.resource.ResourceMgr;
+import workbench.sql.preparedstatement.StatementParameters;
+import workbench.util.SqlUtil;
+
+/**
+ * @author  info@sql-workbench.net
+ */
+public class StatementParameterTableModel
+	implements TableModel
+{
+	private String[] columns = { ResourceMgr.getString("TxtPSParameterIndex"), ResourceMgr.getString("TxtPSParameterType"), ResourceMgr.getString("TxtPSParameterValue") };
+	private Integer[] parameterIndex;
+	private String[] types;
+	private String[] values;
+	private StatementParameters parms;
+	
+	public StatementParameterTableModel(StatementParameters parm)
+	{
+		this.parms = parm;
+		int count = parm.getParameterCount();
+		this.parameterIndex = new Integer[count];
+		this.types = new String[count];
+		this.values = new String[count];
+		for (int i=0; i < count; i++)
+		{
+			this.parameterIndex[i] = new Integer(i+1);
+			this.types[i] = SqlUtil.getTypeName(parm.getParameterType(i));
+			Object v = parm.getParameterValue(i);
+			this.values[i] = (v == null ? "" : v.toString());
+		}
+	}
+
+	public void addTableModelListener(javax.swing.event.TableModelListener l)
+	{
+	}
+
+	public Class getColumnClass(int columnIndex)
+	{
+		if (columnIndex == 0)
+		{
+			return Integer.class;
+		}
+		else 
+		{
+			return String.class;
+		}
+	}
+
+	public int getColumnCount()
+	{
+		return 3;
+	}
+
+	public String getColumnName(int columnIndex)
+	{
+		return columns[columnIndex];
+	}
+
+	public int getRowCount()
+	{
+		return this.parms.getParameterCount();
+	}
+
+	public String getParameterValue(int index)
+	{
+		return this.values[index];
+	}
+	
+	public Object getValueAt(int rowIndex, int columnIndex)
+	{
+		if (columnIndex == 0)
+		{
+			return parameterIndex[rowIndex];
+		}
+		else if (columnIndex == 1)
+		{
+			return types[rowIndex];
+		}
+		else 
+		{
+			return values[rowIndex];
+		}
+	}
+
+	public boolean isCellEditable(int rowIndex, int columnIndex)
+	{
+		return (columnIndex == 2);
+	}
+
+	public void removeTableModelListener(javax.swing.event.TableModelListener l)
+	{
+	}
+
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex)
+	{
+		if (columnIndex == 2)
+		{
+			this.values[rowIndex] = (aValue == null ? "" : aValue.toString());
+		}
+	}
+	
+}
