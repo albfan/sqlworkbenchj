f749b206cb58f32615a8f71399e2c53ab8070dc8
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-03-05 20:55:23 +0000

    initial
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@332 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/actions/CheckPreparedStatementsAction.java
@@ -0,0 +1,78 @@
+/*
+ * HighlightCurrentStatement.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.Action;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
+import javax.swing.border.Border;
+
+import workbench.resource.ResourceMgr;
+import workbench.resource.Settings;
+
+/**
+ *	Action to copy the contents of a entry field into the clipboard
+ *	@author  info@sql-workbench.net
+ */
+public class CheckPreparedStatementsAction extends WbAction
+{
+	private Border originalBorder;
+
+	private boolean switchedOn = false;
+	private JCheckBoxMenuItem toggleMenu;
+
+	public CheckPreparedStatementsAction()
+	{
+		super();
+		this.initMenuDefinition("MnuTxtCheckPrepared");
+		this.setMenuItemName(ResourceMgr.MNU_TXT_SQL);
+		//this.setIcon(null);
+		this.switchedOn = Settings.getInstance().getCheckPreparedStatements();
+	}
+
+	public void executeAction(ActionEvent e)
+	{
+		this.setSwitchedOn(!this.switchedOn);
+	}
+
+	public boolean isSwitchedOn() { return this.switchedOn; }
+
+	public void setSwitchedOn(boolean aFlag)
+	{
+		this.switchedOn = aFlag;
+		if (this.toggleMenu != null) this.toggleMenu.setSelected(aFlag);
+		Settings.getInstance().setCheckPreparedStatements(this.switchedOn);
+	}
+
+	public void addToMenu(JMenu aMenu)
+	{
+		if (this.toggleMenu == null)
+		{
+			this.toggleMenu= new JCheckBoxMenuItem();
+			this.toggleMenu.setAction(this);
+			String text = this.getValue(Action.NAME).toString();
+			int pos = text.indexOf('&');
+			if (pos > -1)
+			{
+				char mnemonic = text.charAt(pos + 1);
+				text = text.substring(0, pos) + text.substring(pos + 1);
+				this.toggleMenu.setMnemonic((int)mnemonic);
+			}
+			this.toggleMenu.setText(text);
+			this.toggleMenu.setSelected(this.switchedOn);
+		}
+		aMenu.add(this.toggleMenu);
+	}
+	
+}
