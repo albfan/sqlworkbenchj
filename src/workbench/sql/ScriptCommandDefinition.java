cd5445d6ca5dcdb2e8af4a0574758c2b78b8c717
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-03-21 18:38:13 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@340 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/sql/ScriptCommandDefinition.java
@@ -0,0 +1,43 @@
+/*
+ * ScriptCommandDefinition.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2004, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.sql;
+
+/**
+ * @author  info@sql-workbench.net
+ */
+public class ScriptCommandDefinition
+{
+	private final String command;
+	private final int startPosInScript;
+	private final int endPosInScript;
+	private int indexInScript;
+	
+	public ScriptCommandDefinition(String c, int start, int end)
+	{
+		this(c, start, end, -1);
+	}
+	
+	public ScriptCommandDefinition(String c, int start, int end, int index)
+	{
+		this.command = c;
+		this.startPosInScript = start;
+		this.endPosInScript = end;
+		this.indexInScript = index;
+	}
+	public String getSQL() { return this.command; }
+	public int getStartPositionInScript() { return this.startPosInScript; }
+	public int getEndPositionInScript() { return this.endPosInScript; }
+	public int getIndexInScript() { return this.indexInScript; }
+	public void setIndexInScript(int index) { this.indexInScript = index; }
+	
+	public String toString() { return this.command; }
+}
