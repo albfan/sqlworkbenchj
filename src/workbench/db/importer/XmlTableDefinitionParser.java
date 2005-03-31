f1dba0fa57955decbeb5de60ce3c60c4637d3fbb
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-03-31 21:37:48 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@355 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/db/importer/XmlTableDefinitionParser.java
@@ -0,0 +1,196 @@
+/*
+ * XmlDataTableParser.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: info@sql-workbench.net
+ *
+ */
+package workbench.db.importer;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import workbench.db.ColumnIdentifier;
+import workbench.db.exporter.XmlRowDataConverter;
+import workbench.log.LogMgr;
+import workbench.util.StrBuffer;
+
+/**
+ * A parser to read the table definition from a Workbench XML file.
+ * @author  info@sql-workbench.net
+ */
+public class XmlTableDefinitionParser
+	extends DefaultHandler
+{
+	private List columnList;
+	private String tableName;
+	private ColumnIdentifier currentColumn;
+	private String filename;
+	private String encoding = "UTF-8";
+	private StringBuffer chars;
+	
+	public XmlTableDefinitionParser(String fname, String enc)
+		throws FileNotFoundException
+	{
+		this.filename = fname;
+		if (enc != null) this.encoding = enc;
+		this.columnList = new ArrayList();
+		this.parseTableStructure();
+	}
+	
+	public ColumnIdentifier[] getColumns()
+	{
+		int count = this.columnList.size();
+		ColumnIdentifier[] cols = new ColumnIdentifier[count];
+		for (int i=0; i < count; i++)
+		{
+			ColumnIdentifier c = (ColumnIdentifier)this.columnList.get(i);
+			cols[i] = c;
+		}
+		return cols;
+	}
+	
+	public String getTableName()
+	{
+		return this.tableName;
+	}
+
+	private void parseTableStructure()
+		throws FileNotFoundException
+	{
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		factory.setValidating(false);
+		InputStream in = null;
+		try
+		{
+			SAXParser saxParser = factory.newSAXParser();
+			in = new FileInputStream(this.filename);
+			InputSource source = new InputSource(new BufferedReader(new InputStreamReader(in, this.encoding)));
+			saxParser.parse(source, this);
+		}
+		catch (ParsingEndedException e)
+		{
+			// ignore this...
+		}
+		catch (FileNotFoundException e)
+		{
+			throw e;
+		}
+		catch (Throwable e)
+		{
+			LogMgr.logError("XmlDataTableParser.parseTableStructure()", "Error reading table structure", e);
+			this.columnList = null;
+			this.tableName = null;
+		}
+		finally
+		{
+		}
+	}
+	
+	public void startElement(String namespaceURI, String sName, String qName, Attributes attrs)
+		throws SAXException
+	{
+		this.chars = new StringBuffer();
+		if (qName.equals(XmlRowDataConverter.COLUMN_DEF_TAG))
+		{
+			this.currentColumn = new ColumnIdentifier();
+		}
+	}
+	
+	public void endElement(String namespaceURI, String sName, String qName)
+		throws SAXException
+	{
+		if (qName.equals(XmlRowDataConverter.COLUMN_DEF_TAG))
+		{
+			this.columnList.add(this.currentColumn);
+		}
+		else if (qName.equals(XmlRowDataConverter.TABLE_NAME_TAG))
+		{
+			this.tableName = this.chars.toString();
+		}
+		else if (qName.equals(XmlRowDataConverter.COLUMN_NAME_TAG))
+		{
+			this.currentColumn.setColumnName(this.chars.toString()); 
+		}
+		else if (qName.equals(XmlRowDataConverter.JAVA_TYPE_TAG))
+		{
+			try
+			{
+				int type = Integer.parseInt(this.chars.toString());
+				this.currentColumn.setDataType(type);
+			}
+			catch (Exception e)
+			{
+				LogMgr.logError("XmlTableDefinitionParser.endElement()", "Could not read columnn type!", e);
+				throw new SAXException("Could not read columnn type");
+			}
+		}
+		else if (qName.equals("dbms-data-type"))
+		{
+			try
+			{
+				this.currentColumn.setDbmsType(this.chars.toString());
+			}
+			catch (Exception e)
+			{
+				LogMgr.logError("XmlDataFileParser.endElement()", "Could not read dbms columnn type!", e);
+				throw new SAXException("Could not read dbms columnn type");
+			}
+		}
+		else if (qName.equals(XmlRowDataConverter.JAVA_CLASS_TAG))
+		{
+			try
+			{
+				this.currentColumn.setColumnClass(this.chars.toString());
+			}
+			catch (Exception e)
+			{
+				LogMgr.logError("XmlTableDefinitionParser.endElement()", "Could not read columnn class name!", e);
+				throw new SAXException("Could not read columnn name");
+			}
+		}
+		else if (qName.equals(XmlRowDataConverter.TABLE_DEF_TAG))
+		{
+			throw new ParsingEndedException();
+		}
+	}	
+	
+	public void characters(char buf[], int offset, int len)
+		throws SAXException
+	{
+		if (chars != null)
+		{
+			this.chars.append(buf, offset, len);
+		}
+	}		
+
+	public static void main(String args[])
+	{
+		try
+		{
+			XmlTableDefinitionParser parser = new XmlTableDefinitionParser("d:/projects/jworkbench/testdata/mind-50.xml", "UTF-8");
+			ColumnIdentifier[] cols = parser.getColumns();
+			System.out.println("table=" + parser.getTableName());
+			for (int i=0; i<cols.length; i++) System.out.println(cols[i]);
+		}
+		catch (Throwable th)
+		{
+			th.printStackTrace();
+		}
+		System.out.println("Done.");
+	}
+}
