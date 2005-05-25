71c314a3bc21cbf3268f17fd756aff65c8e0dd60
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-05-25 22:36:46 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@391 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/db/ErrorInformationReader.java
@@ -0,0 +1,19 @@
+/*
+ * ErrorInformationReader.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ */
+package workbench.db;
+
+/**
+ * @author support@sql-workbench.net
+ */
+public interface ErrorInformationReader
+{
+	String getErrorInfo(String schema, String object, String type);
+}
