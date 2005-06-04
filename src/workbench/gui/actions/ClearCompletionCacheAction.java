1527c03bd71342a145d590e295c332ec754b2fcd
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-06-04 11:07:03 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@394 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/actions/ClearCompletionCacheAction.java
@@ -0,0 +1,43 @@
+/*
+ * AutoCompletionAction.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+import workbench.db.WbConnection;
+import workbench.resource.ResourceMgr;
+
+/**
+ * @author  support@sql-workbench.net
+ */
+public class ClearCompletionCacheAction
+	extends WbAction
+{
+	private WbConnection dbConnection;
+	
+	public ClearCompletionCacheAction()
+	{
+		this.initMenuDefinition("MnuTxtClearCompletionCache");
+		this.setMenuItemName(ResourceMgr.MNU_TXT_SQL);
+		this.setEnabled(false);
+	}
+
+	public void setConnection(WbConnection conn)
+	{
+		this.dbConnection = conn;
+		this.setEnabled(this.dbConnection != null);
+	}
+	
+	public void executeAction(ActionEvent e)
+	{
+		if (this.dbConnection != null) this.dbConnection.getObjectCache().clear();
+	}
+}
---
+++ src/workbench/interfaces/StatementRunner.java
@@ -0,0 +1,35 @@
+/*
+ * StatementRunner.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ */
+package workbench.interfaces;
+
+import java.sql.SQLException;
+import workbench.db.WbConnection;
+import workbench.sql.StatementRunnerResult;
+import workbench.storage.RowActionMonitor;
+
+/**
+ * @author support@sql-workbench.net
+ */
+public interface StatementRunner
+{
+	void setConnection(WbConnection aConn);
+	void setExecutionController(ExecutionController control);
+	StatementRunnerResult getResult();
+	void setResultLogger(ResultLogger logger);
+	void runStatement(String aSql, int maxRows, int queryTimeout)
+		throws SQLException, Exception;
+	void setVerboseLogging(boolean flag);
+	boolean getVerboseLogging();
+	void cancel();
+	void done();
+	void setRowMonitor(RowActionMonitor monitor);
+	void statementDone();
+}
