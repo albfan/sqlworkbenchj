6af0f77b8dd0f5785d796eeeddcae0803ab121c7
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-08-14 23:40:56 +0000

    *** empty log message ***
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@410 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/interfaces/CriteriaPanel.java
@@ -0,0 +1,26 @@
+/*
+ * CriteriaPanel.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ */
+package workbench.interfaces;
+
+import workbench.gui.actions.WbAction;
+
+/**
+ * @author support@sql-workbench.net
+ */
+public interface CriteriaPanel
+{
+	void setFocusToEntryField();
+	String getText();
+	void setText(String aText);
+	void addToToolbar(WbAction action, boolean atFront, boolean withSep);
+	void saveSettings();
+	void restoreSettings();
+}
---
+++ src/workbench/storage/filter/ContainsNotComparator.java
@@ -0,0 +1,32 @@
+/*
+ * ContainsNotComparator.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.storage.filter;
+
+/**
+ * @author support@sql-workbench.net
+ */
+public class ContainsNotComparator
+	extends ContainsComparator
+{
+	public ContainsNotComparator()
+	{
+	}
+
+	public String getName() { return "Contains not"; }
+	public String getOperator() { return "contains not"; }
+	
+	public boolean evaluate(Object reference, Object value, boolean ignoreCase)
+	{
+		return !super.evaluate(reference, value, ignoreCase);
+	}
+}
+
