b2ee683afe0c47e57ff2a1c3f018738e4e2e2901
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-09-09 23:16:14 +0000

    Streamline settings storage
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@431 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/interfaces/PropertyStorage.java
@@ -0,0 +1,25 @@
+/*
+ * PropertyStorage.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.interfaces;
+
+/**
+ * @author support@sql-workbench.net
+ */
+public interface PropertyStorage
+{
+	Object setProperty(String property, String value);
+	void setProperty(String property, int value);
+	void setProperty(String property, boolean value);
+	boolean getBoolProperty(String property, boolean defaultValue);
+	int getIntProperty(String property, int defaultValue);
+	String getProperty(String property, String defaultValue);
+}
