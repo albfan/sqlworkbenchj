9fec74ea19da7f5152e264e046b2b2beea8c7de6
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-11-11 22:14:07 +0000

    For ProcedureListPanel
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@457 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/renderer/ProcStatusRenderer.java
@@ -0,0 +1,48 @@
+/*
+ * RowStatusRenderer.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.gui.renderer;
+
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import workbench.db.JdbcProcedureReader;
+
+/**
+ *
+ * @author  support@sql-workbench.net
+ */
+public class ProcStatusRenderer 
+	extends DefaultTableCellRenderer
+{
+	/** Creates a new instance of NumberColumnRenderer */
+	public ProcStatusRenderer()
+	{
+		this.setHorizontalAlignment(JLabel.LEFT);
+	}
+	
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+	{
+		try
+		{
+			Integer status = (Integer)value;
+			String display = JdbcProcedureReader.convertProcType(status.intValue());
+			return super.getTableCellRendererComponent(table, display, isSelected, hasFocus, row, column);
+		}
+		catch (Exception e)
+		{
+			return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		}
+		
+	}
+	
+}
