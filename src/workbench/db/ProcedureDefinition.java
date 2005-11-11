53ce24a394f3e7a17c6731f0b40819da23a0a444
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-11-11 22:14:38 +0000

    a procedure name
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@458 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/db/ProcedureDefinition.java
@@ -0,0 +1,48 @@
+/*
+ * ProcedureDefinition.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.db;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public class ProcedureDefinition
+{
+	private String schema;
+	private String catalog;
+	private String procName;
+	
+	// as returned by the JDBC driver corresponds to 
+	// DatabaseMetadata.DatabaseMetaData.procedureNoResult
+	// DatabaseMetadata.DatabaseMetaData.procedureReturnsResult
+	private int resultType; 
+	private String typeDefinition;
+	
+	public ProcedureDefinition(String cat, String schem, String name, int type)
+	{
+		schema = schem;
+		catalog = cat;
+		procName = name;
+		resultType = type;
+	}
+	
+	public String getCatalog() { return this.catalog; }
+	public String getSchema() { return this.schema; }
+	public String getProcedureName() { return this.procName; }
+	public int getResultType() { return this.resultType; }
+	
+	public String toString()
+	{
+		return procName;
+	}
+	
+}
