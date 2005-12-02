9a910d40b74d7e2da199e550de21e1b62aee0c08
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-12-02 16:36:16 +0000

    Fixed generation of SQL Statements
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@464 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/storage/ColumnData.java
@@ -0,0 +1,41 @@
+/*
+ * ColumnData.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.storage;
+
+import workbench.db.ColumnIdentifier;
+
+/**
+ * A wrapper class do hold the current value of a column
+ * and it's definition.
+ * The column definition is represented by a {@link workbench.db.ColumnIdentifier}
+ * The value can be any Java object
+ * @author support@sql-workbench.net
+ */
+public class ColumnData
+{
+	private Object data;
+	private ColumnIdentifier id;
+	
+	/**
+	 * Creates a new instance of ColumnData
+	 * @param value The current value of the column
+	 * @param colid The definition of the column
+	 */
+	public ColumnData(Object value, ColumnIdentifier colid)
+	{
+		data = value;
+		id = colid;
+	}
+	
+	public Object getValue() { return data; }
+	public ColumnIdentifier getIdentifier() { return id; }
+}
