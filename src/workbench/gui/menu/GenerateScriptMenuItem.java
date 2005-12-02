84ce068288e638358a1253c60cfabec9e95c2ae2
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-12-02 16:37:51 +0000

    .
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@466 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/menu/GenerateScriptMenuItem.java
@@ -0,0 +1,35 @@
+/*
+ * GenerateScriptMenuItem.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.gui.menu;
+
+import workbench.gui.components.WbMenuItem;
+import workbench.resource.ResourceMgr;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public class GenerateScriptMenuItem
+	extends WbMenuItem
+{
+	public static final String SCRIPT_CMD = "create-scripts";
+	
+	public GenerateScriptMenuItem()
+	{
+		super(ResourceMgr.getString("MnuTxtCreateScript"));
+		setIcon(ResourceMgr.getImage("script"));
+		setActionCommand(SCRIPT_CMD);
+		setEnabled(true);
+		setToolTipText(ResourceMgr.getDescription("MnuTxtCreateScript"));
+	}
+	
+}
