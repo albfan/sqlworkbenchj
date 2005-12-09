a98614ca3412c48ce570b60068d58378cac91809
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2005-12-09 21:57:36 +0000

    .
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@469 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/interfaces/ErrorReporter.java
@@ -0,0 +1,22 @@
+/*
+ * ErrorReporter.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.interfaces;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public interface ErrorReporter
+{
+	void addError(String msg);
+	void addWarning(String warning);
+}
