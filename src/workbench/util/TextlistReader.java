dbd606033e1e19296f6b0cee751000f24445324b
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2006-02-10 19:01:14 +0000

    utility class
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@488 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/util/TextlistReader.java
@@ -0,0 +1,63 @@
+/*
+ * TextlistReader.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2006, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import workbench.log.LogMgr;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public class TextlistReader
+{
+	private List values;
+	/**
+	 * Reads each line of the passed input stream into 
+	 * an element of the internal values collection. 
+	 * Elements will be trimmed.
+	 */
+	public TextlistReader(InputStream in)
+	{
+		try
+		{
+			values = new LinkedList();
+			BufferedReader r = new BufferedReader(new InputStreamReader(in));
+			String line = r.readLine();
+			while (line != null)
+			{
+				values.add(line);
+				line = r.readLine();
+			}
+			//LogMgr.logDebug("TextlistReader.<init>", values.size() + " element read from InputStream");
+		}
+		catch (Exception e)
+		{
+			LogMgr.logError("TestlistReader.<init>", "Error reading input stream", e);
+		}
+		finally
+		{
+			try { in.close(); } catch (Throwable th) {}
+		}
+	}
+	
+	public Collection getValues()
+	{
+		return values;
+	}
+	
+}
