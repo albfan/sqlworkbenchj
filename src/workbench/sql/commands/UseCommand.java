b658bb180867d38061c74470d08f34e8ef577ef9
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2006-04-17 14:54:55 +0000

    Use setCatalog()
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@498 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/sql/commands/UseCommand.java
+++ src/workbench/sql/commands/UseCommand.java
@@ -12,15 +12,21 @@
 package workbench.sql.commands;
 
 import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import workbench.db.WbConnection;
 import workbench.util.ExceptionUtil;
 import workbench.resource.ResourceMgr;
 import workbench.sql.SqlCommand;
 import workbench.sql.StatementRunnerResult;
+import workbench.util.StringUtil;
 
 /**
- * MS SQL Server's USE command. 
+ * MS SQL Server's and MySQL's USE command. 
+ * 
+ * Actually this will be in effect if the JDBC driver reports
+ * that catalog's are supported 
  *
  * This class will notify the connection used that the current database has changed
  * so that the connection display in the main window can be updated.
@@ -40,21 +46,23 @@ public class UseCommand extends SqlCommand
 		try
 		{
 			String oldCatalog = aConnection.getMetadata().getCurrentCatalog();
-			this.currentStatement = aConnection.createStatement();
-			this.currentStatement.execute(aSql);
-
-			StringBuffer warnings = new StringBuffer();
-			if (this.appendWarnings(aConnection, this.currentStatement , warnings))
-			{
-				result.addMessage(warnings.toString());
-			}
-			this.currentStatement.close();
+			
+			Pattern p = Pattern.compile("\\s*USE\\s*", Pattern.CASE_INSENSITIVE);
+			Matcher m = p.matcher(aSql);
+			String catName = m.replaceAll("").trim();
+			aConnection.getSqlConnection().setCatalog(catName);
 			
 			String newCatalog = aConnection.getMetadata().getCurrentCatalog();
 			if (oldCatalog != null && !oldCatalog.equals(newCatalog))
 			{
 				aConnection.catalogChanged(oldCatalog, newCatalog);
 			}
+			String msg = ResourceMgr.getString("MsgCatalogChanged");
+			String term = aConnection.getMetadata().getCatalogTerm();
+			
+			msg = StringUtil.replace(msg, "%newcatalog%", newCatalog);
+			msg = StringUtil.replace(msg, "%catalogterm%", StringUtil.capitalize(term));
+			result.addMessage(msg);
 			result.setSuccess();
 		}
 		catch (Exception e)
