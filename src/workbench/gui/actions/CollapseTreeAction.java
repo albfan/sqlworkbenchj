a1d1d4c6adbc30c8b8af14344a6b381554f7a623
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2006-07-29 09:01:07 +0000

    no message
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@527 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/actions/CollapseTreeAction.java
@@ -0,0 +1,39 @@
+/*
+ * ExpandTreeAction.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+import workbench.gui.profiles.ProfileTree;
+import workbench.resource.ResourceMgr;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public class CollapseTreeAction
+	extends WbAction
+{
+	private ProfileTree client;
+	
+	public CollapseTreeAction(ProfileTree tree)
+	{
+		super();
+		this.client = tree;
+		this.initMenuDefinition("LblCollapseAll");
+		this.setIcon(ResourceMgr.getImage("collapse"));	
+	}
+
+	public void executeAction(ActionEvent e)
+	{
+		this.client.collapseAll();
+	}	
+}
---
+++ src/workbench/gui/actions/ExpandTreeAction.java
@@ -0,0 +1,39 @@
+/*
+ * ExpandTreeAction.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2005, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+import workbench.gui.profiles.ProfileTree;
+import workbench.resource.ResourceMgr;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public class ExpandTreeAction
+	extends WbAction
+{
+	private ProfileTree client;
+	
+	public ExpandTreeAction(ProfileTree tree)
+	{
+		super();
+		this.client = tree;
+		this.initMenuDefinition("LblExpandAll");
+		this.setIcon(ResourceMgr.getImage("expand"));	
+	}
+
+	public void executeAction(ActionEvent e)
+	{
+		this.client.expandAll();
+	}	
+}
