8811c48bf1b2350920d9a508ce93f259c69c8872
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2006-09-14 19:09:13 +0000

    no message
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@577 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/gui/actions/InsertTabAction.java
@@ -0,0 +1,43 @@
+/*
+ * AddTabAction.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2006, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.gui.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.KeyStroke;
+
+import workbench.gui.MainWindow;
+import workbench.resource.ResourceMgr;
+
+/**
+ *	@author  support@sql-workbench.net
+ */
+public class InsertTabAction extends WbAction
+{
+	private MainWindow client;
+
+	public InsertTabAction(MainWindow aClient)
+	{
+		super();
+		this.client = aClient;
+		this.setMenuItemName(ResourceMgr.MNU_TXT_VIEW);
+		this.initMenuDefinition("MnuTxtInsTab");
+		this.setIcon(null);
+	}
+
+	public void executeAction(ActionEvent e)
+	{
+		this.client.insertTab();
+	}
+}
