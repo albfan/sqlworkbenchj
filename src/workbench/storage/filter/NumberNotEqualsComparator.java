56c4eede1979900d4b7198e4e62242a03914875b
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2006-11-02 21:25:46 +0000

    .
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@600 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/storage/filter/NumberNotEqualsComparator.java
@@ -0,0 +1,62 @@
+/*
+ * NumberEqualsComparator.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2006, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.storage.filter;
+
+import workbench.util.StringUtil;
+
+/**
+ * @author support@sql-workbench.net
+ */
+public class NumberNotEqualsComparator
+	implements ColumnComparator
+{
+	public NumberNotEqualsComparator()
+	{
+	}
+	
+	public boolean supportsIgnoreCase() { return false; }
+
+	public String getValueExpression(Object value) { return (value == null ? "" : value.toString()); }
+	public String getOperator() { return "<>"; }
+	public boolean needsValue() { return true; }
+	
+	public boolean evaluate(Object reference, Object value, boolean ignoreCase)
+	{
+		if (reference == null && value == null) return false;
+		if (reference == null && value != null) return true;
+		if (reference != null && value == null) return true;
+		try
+		{
+			return !reference.equals(value);
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+	
+	public boolean supportsType(Class valueClass)
+	{
+		return (Number.class.isAssignableFrom(valueClass));
+	}
+
+	public boolean equals(Object other)
+	{
+		return (other instanceof NumberEqualsComparator);
+	}
+	
+	public boolean validateInput(String value)
+	{
+		return StringUtil.isNumber(value);
+	}	
+
+}
