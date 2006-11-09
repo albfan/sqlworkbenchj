c2d9bcaa2249e05dc852557c30d9f8b4d99d4092
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2006-11-09 23:01:05 +0000

    Support date columns
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@601 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/storage/filter/DateEqualsComparator.java
@@ -0,0 +1,80 @@
+/*
+ * NumberEqualsComparator.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2006, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.storage.filter;
+
+import workbench.util.ValueConverter;
+
+/**
+ * @author support@sql-workbench.net
+ */
+public class DateEqualsComparator
+	implements ColumnComparator
+{
+	private int sqlType;
+	
+	public DateEqualsComparator()
+	{
+		this.sqlType = java.sql.Types.TIMESTAMP;
+	}
+	
+	public DateEqualsComparator(int type)
+	{
+		this.sqlType = type;
+	}
+	
+	public boolean supportsIgnoreCase() { return false; }
+
+	public String getValueExpression(Object value) { return (value == null ? "" : value.toString()); }
+	public String getOperator() { return "="; }
+	public boolean needsValue() { return true; }
+	public boolean comparesEquality() { return true; }
+	
+	public boolean evaluate(Object reference, Object value, boolean ignoreCase)
+	{
+		if (reference == null && value == null) return true;
+		if (reference == null && value != null) return false;
+		if (reference != null && value == null) return false;
+		try
+		{
+			return reference.equals(value);
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+	
+	public boolean supportsType(Class valueClass)
+	{
+		return (java.util.Date.class.isAssignableFrom(valueClass));
+	}
+
+	public boolean equals(Object other)
+	{
+		return (other instanceof NumberEqualsComparator);
+	}
+	
+	public boolean validateInput(String value)
+	{
+		ValueConverter converter = new ValueConverter();
+		try
+		{
+			converter.convertValue(value, this.sqlType);
+			return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}	
+
+}
