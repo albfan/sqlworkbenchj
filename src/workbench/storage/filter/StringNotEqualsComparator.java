b545c9f3151eb8c7517d6d82910e2a325fe6e6b5
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2006-11-09 23:01:50 +0000

    .
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@602 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/storage/filter/StringNotEqualsComparator.java
@@ -0,0 +1,59 @@
+/*
+ * StringEqualsComparator.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2006, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.storage.filter;
+
+/**
+ * @author support@sql-workbench.net
+ */
+public class StringNotEqualsComparator
+	implements ColumnComparator
+{
+	public StringNotEqualsComparator()
+	{
+	}
+	
+	public boolean supportsIgnoreCase() { return true; }
+	public String getValueExpression(Object value) { return "'" + value + "'";}
+	public String getOperator() { return "<>"; }
+	public boolean needsValue() { return true; }
+	public boolean validateInput(String value) { return true; }
+	public boolean comparesEquality() { return false; }
+	
+	public boolean evaluate(Object reference, Object value, boolean ignoreCase)
+	{
+		if (reference == null && value == null) return false;
+		if (reference == null && value != null) return true;
+		if (reference != null && value == null) return true;
+		try
+		{
+			if (ignoreCase)
+				return !((String)reference).equalsIgnoreCase((String)value);
+			else
+				return !reference.equals(value);
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+	
+	public boolean supportsType(Class valueClass)
+	{
+		return (String.class.isAssignableFrom(valueClass));
+	}
+
+	public boolean equals(Object other)
+	{
+		return (other instanceof StringEqualsComparator);
+	}
+	
+}
