be841f7476c95487179133a33094d76451379468
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2006-11-14 19:25:04 +0000

    For schemaDiff
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@609 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/db/NoConfigException.java
@@ -0,0 +1,30 @@
+/*
+ * NoConfigException.java
+ *
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2006, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.db;
+
+/**
+ * @author support@sql-workbench.net
+ */
+public class NoConfigException 
+	extends java.lang.Exception
+{
+	
+	public NoConfigException()
+	{
+	}
+	
+	public NoConfigException(String msg)
+	{
+		super(msg);
+	}
+}
