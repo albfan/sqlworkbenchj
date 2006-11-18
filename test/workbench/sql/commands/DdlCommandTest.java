cd3c4118f0f298cc5279ff07d46b61c388daee0e
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2006-11-18 10:12:46 +0000

    Test
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@613 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ test/workbench/sql/commands/DdlCommandTest.java
@@ -0,0 +1,73 @@
+/*
+ * DdlCommandTest.java
+ * JUnit based test
+ *
+ * Created on November 17, 2006, 11:09 AM
+ */
+
+package workbench.sql.commands;
+
+import junit.framework.TestCase;
+import junit.framework.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import workbench.db.WbConnection;
+import workbench.util.ExceptionUtil;
+import workbench.log.LogMgr;
+import workbench.resource.ResourceMgr;
+import workbench.sql.SqlCommand;
+import workbench.sql.StatementRunnerResult;
+import workbench.util.SqlUtil;
+import workbench.util.StringUtil;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public class DdlCommandTest 
+	extends TestCase
+{
+	
+	public DdlCommandTest(String testName)
+	{
+		super(testName);
+	}
+
+	public void testGetTypeAndObject() throws Exception
+	{
+		try
+		{
+			// detection of the type is already tested for SqlUtil.getCreateType()
+			// so we only need to test getName();
+			String sql = "-- test\ncreate or \t replace\n\nprocedure bla";
+			String name = DdlCommand.CREATE.getObjectName(sql);
+			assertEquals("bla", name);
+			
+			sql = "-- test\ncreate \n\ntrigger test_trg for mytable";
+			name = DdlCommand.CREATE.getObjectName(sql);
+			assertEquals("test_trg", name);
+			
+			sql = "-- test\ncreate function \n\n myfunc\n as something";
+			name = DdlCommand.CREATE.getObjectName(sql);
+			assertEquals("myfunc", name);
+
+			sql = "-- test\ncreate or replace package \n\n some_package \t\t\n as something";
+			name = DdlCommand.CREATE.getObjectName(sql);
+			assertEquals("some_package", name);
+			
+			sql = "-- test\ncreate package body \n\n some_body \t\t\n as something";
+			name = DdlCommand.CREATE.getObjectName(sql);
+			assertEquals("some_body", name);
+			
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	
+}
