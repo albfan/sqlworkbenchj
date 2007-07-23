a099f4959b5a5e58a2a710daa3a1d48a236f7329
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2007-07-23 18:50:28 +0000

    no message
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@734 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/resource/ColumnSortType.java
@@ -0,0 +1,22 @@
+/*
+ * ColumnSortType.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2007, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.resource;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public enum ColumnSortType
+{
+	name,
+	position
+}
