7de7bd339818c63a1dc83ae8f666f6a0eea8cd83
thomas <thomas@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2007-11-29 22:24:08 +0000

    no message
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@808 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ test/junit/workbench/db/importer/modifier/RegexModifierTest.java
@@ -0,0 +1,43 @@
+/*
+ * RegexModifierTest.java
+ * 
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ * 
+ * Copyright 2002-2007, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author.
+ * 
+ * To contact the author please send an email to: support@sql-workbench.net
+ */
+package workbench.db.importer.modifier;
+
+import junit.framework.TestCase;
+import workbench.db.ColumnIdentifier;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public class RegexModifierTest
+	extends TestCase
+{
+	public RegexModifierTest(String testName)
+	{
+		super(testName);
+	}
+
+	public void testModifyValue()
+	{
+		RegexModifier modifier = new RegexModifier();
+		
+		ColumnIdentifier fname = new ColumnIdentifier("fname");
+		ColumnIdentifier lname = new ColumnIdentifier("lname");
+		modifier.addDefinition(fname, "bronx", "brox");
+		
+		String modified = modifier.modifyValue(fname, "Zaphod Beeblebronx");
+		assertEquals("Zaphod Beeblebrox", modified);
+		
+		modified = modifier.modifyValue(lname, "Zaphod Beeblebronx");
+		assertEquals("Zaphod Beeblebronx", modified);
+		
+	}
+}
