0962c9b9c959eb011ea69f88d3bd5dd225179263
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2009-01-01 15:06:38 +0000

    Updated copyright
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@973 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- test/junit/workbench/storage/ColumnRemoverTest.java
+++ test/junit/workbench/storage/ColumnRemoverTest.java
@@ -1,6 +1,13 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * ColumnRemoverTest.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2009, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
  */
 package workbench.storage;
 
