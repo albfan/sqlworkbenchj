befc4237ec6f13a680ecfb58a69c30d2bf306198
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2009-01-01 15:25:06 +0000

    Updated copyright
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@985 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/gui/components/SelectionDisplay.java
+++ src/workbench/gui/components/SelectionDisplay.java
@@ -1,9 +1,9 @@
 /*
- * SelectioinDisplay.java
+ * SelectionDisplay.java
  *
  * This file is part of SQL Workbench/J, http://www.sql-workbench.net
  *
- * Copyright 2002-2008, Thomas Kellerer
+ * Copyright 2002-2009, Thomas Kellerer
  * No part of this code maybe reused without the permission of the author
  *
  * To contact the author please send an email to: support@sql-workbench.net
