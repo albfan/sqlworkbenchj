5052dd11efec2476f2f70cef40a97b2084ed2107
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2009-01-01 15:09:41 +0000

    Updated copyright
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@974 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/console/ResultSetPrinter.java
+++ src/workbench/console/ResultSetPrinter.java
@@ -3,13 +3,12 @@
  *
  * This file is part of SQL Workbench/J, http://www.sql-workbench.net
  *
- * Copyright 2002-2008, Thomas Kellerer
+ * Copyright 2002-2009, Thomas Kellerer
  * No part of this code maybe reused without the permission of the author
  *
  * To contact the author please send an email to: support@sql-workbench.net
  *
  */
-
 package workbench.console;
 
 import java.sql.Types;
