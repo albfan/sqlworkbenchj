e58942d3c0c996037213c9dba39c113e694dae38
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2009-01-01 15:21:54 +0000

    Updated copyright
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@982 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/util/Types40.java
+++ src/workbench/util/Types40.java
@@ -1,9 +1,9 @@
 /*
- * Jdbc40Types.java
+ * Types40.java
  *
  * This file is part of SQL Workbench/J, http://www.sql-workbench.net
  *
- * Copyright 2002-2008, Thomas Kellerer
+ * Copyright 2002-2009, Thomas Kellerer
  * No part of this code maybe reused without the permission of the author
  *
  * To contact the author please send an email to: support@sql-workbench.net
