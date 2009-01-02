50310356cdcec3b57378798d4fd9d07652bf39ff
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2009-01-02 15:48:07 +0000

    Support WORKBENCH_JDK and JAVA_HOME for *nix shell scripts
    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@991 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- scripts/sqlwbconsole.sh
+++ scripts/sqlwbconsole.sh
@@ -2,6 +2,26 @@
 # Start SQL Workbench/J in console mode
 scriptpath=`dirname $0`
 
-java -Djava.awt.headless=true \
-     -Xmx256m \
-     -cp $scriptpath/sqlworkbench.jar:$scriptpath/poi.jar workbench.console.SQLConsole $@ 
+JAVA_BIN=""
+
+if [ -n "$WORKBENCH_JDK" ]
+then
+  JAVA_BIN=$WORKBENCH_JDK
+fi
+
+if [ -z "$JAVA_BIN" ]
+then
+  JAVA_BIN=$JAVA_HOME
+fi
+
+JAVACMD="java"
+
+if [ -n "$JAVA_BIN" ]
+then
+  JAVACMD=${JAVA_BIN%/}/bin/java
+fi
+
+echo $JAVACMD
+
+$JAVACMD -Djava.awt.headless=true -Xmx256m \
+         -cp $scriptpath/sqlworkbench.jar:$scriptpath/poi.jar workbench.console.SQLConsole $@ 
--- scripts/sqlworkbench.sh
+++ scripts/sqlworkbench.sh
@@ -2,7 +2,29 @@
 # Start SQL Workbench/J
 scriptpath=`dirname $0`
 
+JAVA_BIN=""
+
+if [ -n "$WORKBENCH_JDK" ]
+then
+  JAVA_BIN=$WORKBENCH_JDK
+fi
+
+if [ -z "$JAVA_BIN" ]
+then
+  JAVA_BIN=$JAVA_HOME
+fi
+
+JAVACMD="java"
+
+if [ -n "$JAVA_BIN" ]
+then
+  JAVACMD=${JAVA_BIN%/}/bin/java
+fi
+
+echo $JAVACMD
+
 # When running in batch mode on a system with no X11
 # installed the option -Djava.awt.headless=true might
 # be needed for some combinations of OS and JDK
-java -Xmx256m -jar $scriptpath/sqlworkbench.jar $@
+
+$JAVACMD -Xmx256m -jar $scriptpath/sqlworkbench.jar $@
