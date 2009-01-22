f754bce6a7db9c34e2250130f5560a90a99d8890
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2009-01-22 21:17:57 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@1029 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/db/CommentSqlManager.java
@@ -0,0 +1,44 @@
+/*
+ * MacroDefinition.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2008, Thomas Kellerer
+ * No part of this code maybe reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.db;
+
+import workbench.resource.Settings;
+import workbench.util.StringUtil;
+
+/**
+ *
+ * @author support@sql-workbench.net
+ */
+public class CommentSqlManager
+{
+	public static final String COMMENT_OBJECT_NAME_PLACEHOLDER = "%object_name%";
+	public static final String COMMENT_SCHEMA_PLACEHOLDER = "%schema%";
+	public static final String COMMENT_COLUMN_PLACEHOLDER = MetaDataSqlManager.COLUMN_NAME_PLACEHOLDER;
+	public static final String COMMENT_PLACEHOLDER = "%comment%";
+
+	private String dbid;
+
+	public CommentSqlManager(WbConnection con)
+	{
+		this.dbid = con.getMetadata().getDbId();
+	}
+
+	public String getCommentSqlTemplate(String objectType)
+	{
+		if (StringUtil.isBlank(objectType)) return null;
+
+		objectType = objectType.toLowerCase().replace(" ", "_");
+		
+		String defaultValue = Settings.getInstance().getProperty("workbench.db.sql.comment." + objectType, null);
+		return Settings.getInstance().getProperty("workbench.db." + dbid + ".sql.comment." + objectType, defaultValue);
+	}
+}
