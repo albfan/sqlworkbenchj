ef658f8c08e6100c96017e48e3a470b7dfd10b0e
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2009-10-04 16:38:37 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@1526 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ test/junit/workbench/db/diff/H2TestTrigger.java
@@ -0,0 +1,39 @@
+/*
+ * H2TestTrigger
+ * 
+ *  This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ * 
+ *  Copyright 2002-2009, Thomas Kellerer
+ *  No part of this code maybe reused without the permission of the author
+ * 
+ *  To contact the author please send an email to: support@sql-workbench.net
+ */
+package workbench.db.diff;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.h2.api.Trigger;
+
+/**
+ *
+ * @author Thomas Kellerer
+ */
+public class H2TestTrigger
+	implements Trigger
+{
+
+	@Override
+	public void init(Connection cnctn, String string, String string1, String string2, boolean bln, int i)
+		throws SQLException
+	{
+		// nothing to do
+	}
+
+	@Override
+	public void fire(Connection cnctn, Object[] os, Object[] os1)
+		throws SQLException
+	{
+		// nothing to do
+	}
+
+}
