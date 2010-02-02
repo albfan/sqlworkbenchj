bea0331fe97a205a92609e52fd9a2e73ef512279
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2010-02-02 21:17:08 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@1717 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ test/junit/workbench/sql/preparedstatement/ParameterDefinitionTest.java
@@ -0,0 +1,53 @@
+/*
+ * ParameterDefinitionTest
+ *
+ *  This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ *  Copyright 2002-2009, Thomas Kellerer
+ *  No part of this code maybe reused without the permission of the author
+ *
+ *  To contact the author please send an email to: support@sql-workbench.net
+ */
+package workbench.sql.preparedstatement;
+
+import java.sql.Types;
+import workbench.WbTestCase;
+
+/**
+ *
+ * @author Thomas Kellerer
+ */
+public class ParameterDefinitionTest
+	extends WbTestCase
+{
+
+	public ParameterDefinitionTest(String testName)
+	{
+		super(testName);
+	}
+
+	public void testParameter()
+	{
+		ParameterDefinition varcharDef = new ParameterDefinition(1, Types.VARCHAR);
+		varcharDef.setParameterName("FIRSTNAME");
+		assertTrue(varcharDef.isValueValid("Test"));
+		assertTrue(varcharDef.isValueValid("5"));
+		assertEquals(1, varcharDef.getIndex());
+		assertEquals(Types.VARCHAR, varcharDef.getType());
+
+		ParameterDefinition intDef = new ParameterDefinition(5, Types.INTEGER);
+		intDef.setParameterName("PERSON_ID");
+		assertFalse(intDef.isValueValid("Test"));
+		assertTrue(intDef.isValueValid("5"));
+		assertEquals(5, intDef.getIndex());
+		assertEquals(Types.INTEGER, intDef.getType());
+
+		ParameterDefinition dateDef = new ParameterDefinition(6, Types.DATE);
+		dateDef.setParameterName("HIRE_DATE");
+		assertTrue(dateDef.isValueValid("2010-01-01"));
+		assertFalse(dateDef.isValueValid("12345"));
+		assertEquals(6, dateDef.getIndex());
+		assertEquals(Types.DATE, dateDef.getType());
+	}
+
+}
---
+++ test/junit/workbench/sql/preparedstatement/StatementParametersTest.java
@@ -0,0 +1,58 @@
+/*
+ * StatementParametersTest
+ * 
+ *  This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ * 
+ *  Copyright 2002-2009, Thomas Kellerer
+ *  No part of this code maybe reused without the permission of the author
+ * 
+ *  To contact the author please send an email to: support@sql-workbench.net
+ */
+package workbench.sql.preparedstatement;
+
+import java.sql.Types;
+import java.util.List;
+import workbench.WbTestCase;
+import workbench.util.CollectionUtil;
+
+/**
+ *
+ * @author Thomas Kellerer
+ */
+public class StatementParametersTest
+	extends WbTestCase
+{
+
+	public StatementParametersTest(String testName)
+	{
+		super(testName);
+	}
+
+	public void testParameters()
+	{
+		List<ParameterDefinition> defs = CollectionUtil.arrayList();
+		ParameterDefinition one = new ParameterDefinition(1, Types.VARCHAR);
+		one.setParameterName("FIRSTNAME");
+		defs.add(one);
+
+		ParameterDefinition two = new ParameterDefinition(2, Types.INTEGER);
+		two.setParameterName("PERSON_ID");
+		defs.add(two);
+
+		StatementParameters params = new StatementParameters(defs);
+		assertEquals(2, params.getParameterCount());
+		
+		assertEquals("FIRSTNAME", params.getParameterName(0));
+		assertEquals(Types.VARCHAR, params.getParameterType(0));
+
+		assertEquals("PERSON_ID", params.getParameterName(1));
+		assertEquals(Types.INTEGER, params.getParameterType(1));
+		
+		params.setParameterValue(0, "Arthur");
+		params.setParameterValue(1, "42");
+		assertEquals("Arthur", params.getParameterValue(0));
+		assertEquals(Integer.valueOf(42), params.getParameterValue(1));
+	}
+
+
+}
