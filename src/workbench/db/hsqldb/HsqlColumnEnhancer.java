2fd50068c384e14858ed1ff15c4c5a299daf9d18
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2010-02-12 20:07:50 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@1743 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/db/hsqldb/HsqlColumnEnhancer.java
+++ src/workbench/db/hsqldb/HsqlColumnEnhancer.java
@@ -44,11 +44,17 @@ public class HsqlColumnEnhancer
 		String tablename = table.getTable().getTableName();
 		String schema = table.getTable().getSchema();
 
-		String sql = "select column_name, is_generated, generation_expression \n" +
-             "from information_schema.columns \n" +
-             "where table_name = ? \n" +
-             "and table_schema = ? \n" +
-             "and is_generated <> 'NEVER' \n";
+		String sql = "select column_name, " +
+								"        generation_expression, " +
+								"        is_generated, " +
+								"        is_identity, " +
+								"        identity_generation, " +
+								"        identity_start, " +
+								"        identity_increment " +
+								"from information_schema.columns \n" +
+								"where table_name = ? \n" +
+								"and table_schema = ? \n" +
+								"and (is_generated = 'ALWAYS' OR (is_identity = 'YES' AND identity_generation IS NOT NULL))  \n";
 
 		Map<String, String> expressions = new HashMap<String, String>();
 		try
@@ -60,9 +66,36 @@ public class HsqlColumnEnhancer
 			while (rs.next())
 			{
 				String colname = rs.getString(1);
-				String genType = rs.getString(2);
-				String expr = rs.getString(3);
-				expressions.put(colname, expr);
+				String computedExpr = rs.getString(2);
+				String isComputed = rs.getString(3);
+				String isIdentity = rs.getString(4);
+				String identityExpr = rs.getString(5);
+
+				String columnExpression = null;
+				if (StringUtil.equalString(isComputed, "ALWAYS"))
+				{
+					// HSQL apparently stores a fully qualified column name
+					// Using that when re-creating the source looks a bit weird, so
+					// we'll simply strip off the schema and table name
+					int pos = computedExpr.lastIndexOf('.');
+					if (pos > 1)
+					{
+						computedExpr = computedExpr.substring(pos + 1);
+					}
+					columnExpression = "GENERATED ALWAYS AS (" + computedExpr + ")";
+				}
+				else if (StringUtil.equalString(isIdentity, "YES"))
+				{
+					String start = rs.getString(6);
+					String inc = rs.getString(7);
+					columnExpression = "GENERATED " + identityExpr + " AS IDENTITY";
+					if (!StringUtil.equalString("0", start) || !StringUtil.equalString("1", inc))
+					{
+						columnExpression += " (START WITH " + start + " INCREMENT BY " + inc + ")";
+					}
+				}
+				if (StringUtil.isBlank(columnExpression)) continue;
+				expressions.put(colname, columnExpression);
 			}
 		}
 		catch (Exception e)
