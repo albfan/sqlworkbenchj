41f2bb55da714f1a9107e3a848c1fb37bff63561
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2010-07-15 12:54:48 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@2002 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- test/junit/workbench/sql/wbcommands/ObjectInfoTest.java
+++ test/junit/workbench/sql/wbcommands/ObjectInfoTest.java
@@ -39,9 +39,11 @@ public class ObjectInfoTest
 		TestUtil util = new TestUtil(getName());
 		db = util.getConnection(getName());
 		TestUtil.executeScript(db,
-				"CREATE TABLE person (nr integer, person_name varchar(100));" +
-				"CREATE VIEW v_person AS SELECT * FROM PERSON;" +
-				"create sequence seq_id;" +
+				"CREATE TABLE person (nr integer primary key, person_name varchar(100)); \n" +
+				"CREATE TABLE person_group (person_nr integer, group_nr integer); \n" +
+				"ALTER TABLE person_group ADD CONSTRAINT fk_pg_p FOREIGN KEY (person_nr) REFERENCES person (nr); \n" +
+				"CREATE VIEW v_person (pnr, pname) AS SELECT nr, person_name FROM PERSON; \n" +
+				"create sequence seq_id; \n" +
 				"commit;"
 		);
 	}
@@ -65,15 +67,39 @@ public class ObjectInfoTest
 		assertEquals("NR", ds.getValueAsString(0, 0));
 		assertEquals("PERSON_NAME", ds.getValueAsString(1, 0));
 
+		tableInfo = info.getObjectInfo(db, objectName, true);
+		assertTrue(tableInfo.hasDataStores());
+		assertEquals(3, tableInfo.getDataStores().size());
+
+		DataStore indexes = tableInfo.getDataStores().get(1);
+		assertEquals("PERSON - Indexes", indexes.getResultName());
+		assertEquals(1, indexes.getRowCount());
+
+		DataStore fk = tableInfo.getDataStores().get(2);
+		assertEquals("PERSON - Referenced by", fk.getResultName());
+		assertEquals(1, fk.getRowCount());
+
+		tableInfo = info.getObjectInfo(db, "PERSON_GROUP", true);
+		assertTrue(tableInfo.hasDataStores());
+		assertEquals(3, tableInfo.getDataStores().size());
+
+		indexes = tableInfo.getDataStores().get(1);
+		assertEquals("PERSON_GROUP - Indexes", indexes.getResultName());
+		assertEquals(1, indexes.getRowCount());
+
+		fk = tableInfo.getDataStores().get(2);
+		assertEquals("PERSON_GROUP - References", fk.getResultName());
+		assertEquals(1, fk.getRowCount());
+		
 		StatementRunnerResult viewInfo = info.getObjectInfo(db, "v_person", false);
 //		System.out.println(viewInfo.getSourceCommand());
 		assertTrue(viewInfo.getSourceCommand().startsWith("CREATE FORCE VIEW"));
 		assertTrue(viewInfo.hasDataStores());
 		
-		DataStore viewDs = tableInfo.getDataStores().get(0);
+		DataStore viewDs = viewInfo.getDataStores().get(0);
 		assertEquals(2, viewDs.getRowCount());
-		assertEquals("NR", viewDs.getValueAsString(0, 0));
-		assertEquals("PERSON_NAME", viewDs.getValueAsString(1, 0));
+		assertEquals("PNR", viewDs.getValueAsString(0, 0));
+		assertEquals("PNAME", viewDs.getValueAsString(1, 0));
 
 		StatementRunnerResult seqInfo = info.getObjectInfo(db, "seq_id", false);
 //		System.out.println(seqInfo.getSourceCommand());
