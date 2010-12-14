820ff6619ea59552c6b3dc9931a7ab698d5eac92
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2010-12-14 14:49:05 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@2216 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- launcher/make32.cmd
+++ launcher/make32.cmd
@@ -1,6 +1,3 @@
-copy ..\src\workbench\resource\images\workbench.ico .
-copy ..\src\workbench\resource\images\console.ico .
-
 rcedit /C SQLWorkbench.exe
 rcedit /I SQLWorkbench.exe workbench.ico
 rcedit /N SQLWorkbench.exe sqlworkbench.ini
--- launcher/make64.cmd
+++ launcher/make64.cmd
@@ -1,9 +1,7 @@
-copy ..\src\workbench\resource\images\workbench.ico .
-copy ..\src\workbench\resource\images\console.ico .
-
 rcedit64 /C SQLWorkbench64.exe
 rcedit64 /I SQLWorkbench64.exe workbench.ico
 rcedit64 /N SQLWorkbench64.exe sqlworkbench.ini
 
-rcedit /I sqlwbconsole64.exe console.ico
-rcedit /N sqlwbconsole64.exe sqlwbconsole.ini
+rcedit64 /C sqlwbconsole64.exe
+rcedit64 /I sqlwbconsole64.exe console.ico
+rcedit64 /N sqlwbconsole64.exe sqlwbconsole.ini
