b9cab26d1baf020d3cafcee95e1bae4f5e9ffc00
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2011-01-22 10:37:28 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@2273 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/db/JdbcTableDefinitionReader.java
+++ src/workbench/db/JdbcTableDefinitionReader.java
@@ -4,7 +4,7 @@
  * This file is part of SQL Workbench/J, http://www.sql-workbench.net
  *
  * Copyright 2002-2011, Thomas Kellerer
- * No part of this code maybe reused without the permission of the author
+ * No part of this code may be reused without the permission of the author
  *
  * To contact the author please send an email to: support@sql-workbench.net
  */
