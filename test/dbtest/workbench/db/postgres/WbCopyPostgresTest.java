1a1b6a25d852fbef6b0224a3040b136c81a754b6
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2011-05-06 17:03:40 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@2442 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- test/dbtest/workbench/db/postgres/WbCopyPostgresTest.java
+++ test/dbtest/workbench/db/postgres/WbCopyPostgresTest.java
@@ -50,10 +50,16 @@ public class WbCopyPostgresTest
 		if (con == null) return;
 
 		TestUtil.executeScript(con,
-			"create table person (id integer, first_name varchar(50), last_name varchar(50));\n" +
+			"create table person (id integer primary key, first_name varchar(50), last_name varchar(50));\n" +
 			"insert into person (id, first_name, last_name) values (1, 'Arthur', 'Dent');\n" +
 			"insert into person (id, first_name, last_name) values (2, 'Ford', 'Prefect');\n" +
-			"commit;\n");
+			"commit;\n" +
+			"create table address (id integer primary key, person_id integer, address_info varchar(100));\n" +
+			"alter table address add constraint fk_person_address foreign key (person_id) references person (id);\n" +
+			"insert into address (id, person_id, address_info) values (1, 1, 'arthur'); \n" +
+			"insert into address (id, person_id, address_info) values (2, 2, 'ford'); \n" +
+			"commit;\n"
+			);
 	}
 
 	@AfterClass
@@ -63,7 +69,7 @@ public class WbCopyPostgresTest
 		Settings.getInstance().setProperty("workbench.sql.ignoreschema.postgresql", "public");
 		PostgresTestUtil.cleanUpTestCase(TEST_ID);
 	}
-	
+
 	@Test
 	public void testTempTarget()
 		throws Exception
@@ -74,9 +80,9 @@ public class WbCopyPostgresTest
 		{
 			WbConnection con = PostgresTestUtil.getPostgresConnection();
 			if (con == null) return;
-			
+
 			con.getMetadata().resetSchemasToIgnores();
-			
+
 			Settings.getInstance().setProperty("workbench.sql.ignoreschema.postgresql", "public," + TEST_ID);
 
 			WbCopy copyCmd = new WbCopy();
@@ -86,7 +92,7 @@ public class WbCopyPostgresTest
 				"create temporary table target_person (id integer, first_name varchar(50), last_name varchar(50));\n " +
 				"commit;\n");
 
-			String sql = 
+			String sql =
 				"wbcopy -sourceQuery='select id, first_name, last_name from person' " +
 				"       -targetTable=target_person -skipTargetCheck=true";
 
@@ -121,7 +127,7 @@ public class WbCopyPostgresTest
 
 			TestUtil util = getTestUtil();
 			WbConnection source = util.getConnection("copyCreateTestSource"); // H2
-			
+
 			WbCopy copyCmd = new WbCopy();
 			copyCmd.setConnection(pgCon);
 
@@ -151,6 +157,81 @@ public class WbCopyPostgresTest
 		{
 			SqlUtil.closeAll(rs, stmt);
 		}
-		
+	}
+
+	@Test
+	public void testFKError()
+		throws Exception
+	{
+		Statement stmt = null;
+		ResultSet rs = null;
+		try
+		{
+			WbConnection pgCon = PostgresTestUtil.getPostgresConnection();
+			if (pgCon == null) return;
+
+			TestUtil util = getTestUtil();
+			WbConnection source = util.getConnection("copyCreateTestSource"); // H2
+
+			WbCopy copyCmd = new WbCopy();
+			copyCmd.setConnection(pgCon);
+
+			TestUtil.executeScript(source,
+				"create table address (id integer primary key, person_id integer, address_info varchar(100));\n" +
+				"insert into address (id, person_id, address_info) values (1, 10, 'arthur'); \n" +
+				"insert into address (id, person_id, address_info) values (2, 2, 'ford'); \n" +
+				"commit;\n");
+
+			TestUtil.executeScript(pgCon,
+				"delete from address;\n" +
+				"commit;\n"
+			);
+
+			String sql =
+				"wbcopy -sourceTable=address " +
+				"       -targetTable=address -mode=insert,update \n"+
+				"       -sourceProfile=" + source.getProfile().getName() + " \n ";
+
+			StatementRunnerResult result = copyCmd.execute(sql);
+			String msg = result.getMessageBuffer().toString();
+			
+			// has to fail because the FK exception should not be ignored!
+			assertEquals(msg, false, result.isSuccess());
+
+			TestUtil.executeScript(pgCon,
+				"delete from address;\n" +
+				"insert into address (id, person_id, address_info) values (2, 2, 'oldvalue'); \n" +
+				"commit;\n"
+			);
+
+			sql =
+				"wbcopy -sourceTable=address -continueOnError=true -useSavepoint=true " +
+				"       -targetTable=address -mode=insert,update \n"+
+				"       -sourceProfile=" + source.getProfile().getName() + " \n ";
+
+			result = copyCmd.execute(sql);
+			msg = result.getMessageBuffer().toString();
+
+			assertEquals(msg, true, result.isSuccess());
+			stmt = pgCon.createStatement();
+			rs = stmt.executeQuery("select count(*) from address");
+			if (rs.next())
+			{
+				int count = rs.getInt(1);
+				assertEquals("Incorrect number of rows copied", 1, count);
+			}
+			rs.close();
+			rs = stmt.executeQuery("select address_info from address where id = 2");
+			if (rs.next())
+			{
+				String value = rs.getString(1);
+				assertEquals("Wrong value imported", "ford", value);
+			}
+		}
+		finally
+		{
+			SqlUtil.closeAll(rs, stmt);
+		}
+
 	}
 }
