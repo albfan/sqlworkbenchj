ca71ff9b829c712459add7b5907189dc81aae765
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2011-07-14 21:40:55 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@2607 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/db/DbSettings.java
+++ src/workbench/db/DbSettings.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import sun.security.action.GetBooleanAction;
+
 import workbench.db.exporter.RowDataConverter;
 import workbench.gui.dbobjects.TableSearchPanel;
 import workbench.log.LogMgr;
