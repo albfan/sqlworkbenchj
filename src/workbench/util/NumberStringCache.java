8ed631b268a33ddc34ca47e5ee3f29d354e0b63d
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2012-02-01 14:48:36 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@2845 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/util/NumberStringCache.java
+++ src/workbench/util/NumberStringCache.java
@@ -17,13 +17,13 @@ package workbench.util;
  */
 public class NumberStringCache
 {
-	// As this is class is used to cache the String representation for 
+	// As this class is used to cache the String representation for
 	// Line numbers in the editor, caching 5000 numbers should suffice
-	// for most cases. 
+	// for most cases.
 	public static final int CACHE_SIZE = 5000;
 	private static final String[] cache = new String[CACHE_SIZE];
 	private static final String[] hexCache = new String[256];
-	
+
 	private NumberStringCache()
 	{
 	}
@@ -41,20 +41,20 @@ public class NumberStringCache
 			{
 				hexCache[value] = Integer.toHexString(value);
 			}
-			
+
 		}
 		return hexCache[value];
 	}
-	
+
 	public static String getNumberString(long lvalue)
 	{
 		if (lvalue < 0 || lvalue >= CACHE_SIZE) return Long.toString(lvalue);
 
 		int value = (int)lvalue;
-		// I'm not synchronizing this, because the worst that can 
+		// I'm not synchronizing this, because the worst that can
 		// happen is, that the same number is created two or three times
-		// instead of exactly one time. 
-		// And as this is most of the time called from Swing Event Thread
+		// instead of exactly one time.
+		// And because this is most of the time called from Swing Event Thread
 		// it is more or less a single-threaded access anyway.
 		if (cache[value] == null)
 		{
@@ -62,5 +62,5 @@ public class NumberStringCache
 		}
 		return cache[value];
 	}
-	
+
 }
