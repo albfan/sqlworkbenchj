f23c57d0f57eccb5f428c98f64d35fb5cff8c82d
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2012-02-21 08:33:24 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@2877 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/gui/actions/FindPreviousAction.java
+++ src/workbench/gui/actions/FindPreviousAction.java
@@ -32,7 +32,7 @@ public class FindPreviousAction
 	{
 		super();
 		this.client = aClient;
-		this.initMenuDefinition("MnuTxtFindPrevious", KeyStroke.getKeyStroke(KeyEvent.VK_F, KeyEvent.SHIFT_MASK));
+		this.initMenuDefinition("MnuTxtFindPrevious", KeyStroke.getKeyStroke(KeyEvent.VK_F3, KeyEvent.SHIFT_MASK));
 		this.setMenuItemName(ResourceMgr.MNU_TXT_EDIT);
 		this.setDescriptiveName(ResourceMgr.getString("TxtEdPrefix") + " " + getMenuLabel());
 	}
