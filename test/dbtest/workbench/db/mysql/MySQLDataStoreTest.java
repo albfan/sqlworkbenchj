5b39897a0f4f92c55e7e46e1d99c86cbe1fad95d
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2012-03-05 21:50:37 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@2914 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ test/dbtest/workbench/db/mysql/MySQLDataStoreTest.java
@@ -0,0 +1,95 @@
+/*
+ * MySQLDataStoreTest.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2012, Thomas Kellerer
+ * No part of this code may be reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ */
+package workbench.db.mysql;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import workbench.TestUtil;
+import workbench.WbTestCase;
+import workbench.db.WbConnection;
+import workbench.storage.DataStore;
+import workbench.util.SqlUtil;
+import static org.junit.Assert.*;
+import workbench.db.TableIdentifier;
+
+/**
+ *
+ * @author Thomas Kellerer
+ */
+public class MySQLDataStoreTest
+	extends WbTestCase
+{
+
+	public MySQLDataStoreTest()
+	{
+		super("MySQLDataStoreTest");
+	}
+
+	@BeforeClass
+	public static void setUpClass()
+		throws Exception
+	{
+		MySQLTestUtil.initTestcase("MySQLDataStoreTest");
+
+		WbConnection con = MySQLTestUtil.getMySQLConnection();
+		if (con == null) return;
+
+		String sql =
+			"CREATE TABLE data ( id integer primary key, info varchar(100));\n"  +
+			"insert into data (id, info) values (1, 'gargleblaster');\n" +
+			"commit;\n";
+		TestUtil.executeScript(con, sql);
+	}
+
+	@AfterClass
+	public static void tearDownClass()
+		throws Exception
+	{
+		WbConnection con = MySQLTestUtil.getMySQLConnection();
+		if (con == null) return;
+		String sql = "DROP TABLE data;";
+		TestUtil.executeScript(con, sql);
+	}
+
+	@Test
+	public void testUpdateColumnDefinition()
+		throws SQLException
+	{
+		WbConnection con = MySQLTestUtil.getMySQLConnection();
+		if (con == null)
+		{
+			return;
+		}
+
+		Statement stmt = null;
+		ResultSet rs = null;
+		try
+		{
+			String sql = "SELECT * from data";
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			DataStore ds = new DataStore(rs, con, true);
+			ds.setGeneratingSql(sql);
+			boolean canUpdate = ds.checkUpdateTable();
+			assertTrue(canUpdate);
+			TableIdentifier table = ds.getUpdateTable();
+			assertEquals("data", table.getTableName());
+		}
+		finally
+		{
+			SqlUtil.closeAll(rs, stmt);
+		}
+	}
+}
