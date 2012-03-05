b8c5b953717bab3598a14b48a2b895c611dfb540
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2012-03-05 23:03:16 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@2916 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ src/workbench/db/mysql/MySQLTableSourceBuilder.java
@@ -0,0 +1,128 @@
+/*
+ * MySQLTableSourceBuilder.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2012, Thomas Kellerer
+ * No part of this code may be reused without the permission of the author
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ */
+package workbench.db.mysql;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import workbench.db.*;
+import workbench.log.LogMgr;
+import workbench.util.SqlUtil;
+import workbench.util.StringUtil;
+
+/**
+ *
+ * @author Thomas Kellerer
+ */
+public class MySQLTableSourceBuilder
+	extends TableSourceBuilder
+{
+
+	public MySQLTableSourceBuilder(WbConnection con)
+	{
+		super(con);
+	}
+
+	@Override
+	protected String getAdditionalTableOptions(TableIdentifier table, List<ColumnIdentifier> columns, List<IndexDefinition> indexList)
+	{
+		if (table == null) return null;
+
+		StringBuilder result = null;
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql =
+			"select engine, \n"  +
+			"       table_comment, \n" +
+			"       table_collation \n" +
+			"from information_schema.tables \n" +
+			"where table_schema = ? \n " +
+			"  and table_name = ? ";
+
+		String defaultCollation = getDefaultCollation(dbConnection);
+		try
+		{
+			pstmt = dbConnection.getSqlConnection().prepareStatement(sql);
+			pstmt.setString(1, table.getCatalog());
+			pstmt.setString(2, table.getRawTableName());
+
+			rs = pstmt.executeQuery();
+			if (rs.next())
+			{
+				result = new StringBuilder(100);
+				appendOption(result, "ENGINE", rs.getString("engine"));
+				String comment = rs.getString("table_comment");
+				if (StringUtil.isNonBlank(comment))
+				{
+					comment = comment.replaceAll("'", "''");
+					comment = "'" + comment + "'";
+					appendOption(result, "COMMENT", comment);
+				}
+				String collation = rs.getString("table_collation");
+				if (!StringUtil.equalStringIgnoreCase(defaultCollation, collation))
+				{
+					appendOption(result, "COLLATE", collation);
+				}
+			}
+		}
+		catch (SQLException ex)
+		{
+			LogMgr.logDebug("MySQLTableSourceBuilder.getAdditionalTableOptions()", "Could not read table status", ex);
+		}
+		finally
+		{
+			SqlUtil.closeAll(rs, pstmt);
+		}
+		if (result == null) return null;
+		return result.toString();
+	}
+
+	private void appendOption(StringBuilder result, String option, String value)
+	{
+		if (StringUtil.isNonEmpty(value))
+		{
+			if (result.length() > 0) result.append('\n');
+			result.append(option);
+			result.append('=');
+			result.append(value);
+		}
+	}
+
+	private String getDefaultCollation(WbConnection con)
+	{
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		String defaultCollation = null;
+		try
+		{
+			stmt = dbConnection.createStatement();
+			rs = stmt.executeQuery("show variables like 'collation_database'");
+			if (rs.next())
+			{
+				defaultCollation = rs.getString("value");
+			}
+		}
+		catch (SQLException ex)
+		{
+			LogMgr.logDebug("MySQLTableSourceBuilder.getDefaultCollation()", "Could not read collation", ex);
+		}
+		finally
+		{
+			SqlUtil.closeAll(rs, stmt);
+		}
+		return defaultCollation;
+
+	}
+}
