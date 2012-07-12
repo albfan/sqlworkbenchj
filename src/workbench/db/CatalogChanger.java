08992c767c8600d5b8f3461c8e039e44b591ce9f
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2012-07-12 08:05:17 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@3223 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/db/CatalogChanger.java
+++ src/workbench/db/CatalogChanger.java
@@ -88,7 +88,7 @@ public class CatalogChanger
 		{
 			conn.catalogChanged(old, newCatalog);
 		}
-		LogMgr.logDebug("DbMetadata.setCurrentCatalog()", "Catalog changed to " + newCat);
+		LogMgr.logDebug("CatalogChanger.setCurrentCatalog()", "Catalog changed to " + newCat);
 
 		return true;
 	}
