dd5af546e05804bb86b2ca308dc6e57fba361236
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2013-02-28 18:56:37 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@3737 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- test/dbtest/workbench/db/mssql/WbExportSqlServerTest.java
+++ test/dbtest/workbench/db/mssql/WbExportSqlServerTest.java
@@ -7,18 +7,22 @@ package workbench.db.mssql;
 import java.io.BufferedReader;
 import java.io.File;
 import java.util.List;
-import org.junit.AfterClass;
-import static org.junit.Assert.*;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import workbench.TestUtil;
 import workbench.WbTestCase;
+
 import workbench.db.WbConnection;
-import workbench.sql.StatementRunnerResult;
-import workbench.sql.wbcommands.WbExport;
+
+import workbench.sql.BatchRunner;
+
 import workbench.util.EncodingUtil;
 import workbench.util.FileUtil;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  *
@@ -42,9 +46,9 @@ public class WbExportSqlServerTest
 		if (con == null) return;
 
 		TestUtil.executeScript(con,
-			"create table data (data_id integer not null primary key, some_data varbinary(64));\n" +
-			"insert int data (data_id, some_data) values (1, 0x01020304);\n" +
-			"insert int data (data_id, some_data) values (2, 0x02030405);\n" +
+			"create table data (some_data binary(4), data_id integer not null);\n" +
+			"insert into data (data_id, some_data) values (1, 0x01020304);\n" +
+			"insert into data (data_id, some_data) values (2, 0x02030405);\n" +
 			"commit;\n"
 			);
 	}
@@ -62,6 +66,9 @@ public class WbExportSqlServerTest
 	public void testBlobExport()
 		throws Exception
 	{
+		WbConnection conn = SQLServerTestUtil.getSQLServerConnection();
+		if (conn == null) return;
+
 		TestUtil util = getTestUtil();
 
 		String dir = util.getBaseDir();
@@ -73,17 +80,16 @@ public class WbExportSqlServerTest
 			"         -encoding='ISO-8859-1'\n" +
 			"         -header=true\n" +
 			"         -blobType=ansi\n" +
-			"         -dateFormat='yyyy-MM-dd';";
-		WbExport command = new WbExport();
+			"         -dateFormat='yyyy-MM-dd';" +
+			" select some_data, data_id from data order by data_id";
 
-		WbConnection conn = SQLServerTestUtil.getSQLServerConnection();
-		if (conn == null) return;
 
-		command.setConnection(conn);
-		StatementRunnerResult result = command.execute(sql);
-		String msg = result.getMessageBuffer().toString();
-		System.out.println(msg);
-		assertTrue(result.isSuccess());
+		BatchRunner runner = new BatchRunner();
+		runner.setConnection(conn);
+		runner.executeScript(sql);
+
+		String msg = runner.getMessages();
+		assertTrue(msg, runner.isSuccess());
 		assertTrue(output.exists());
 		BufferedReader in = null;
 		try
@@ -92,10 +98,13 @@ public class WbExportSqlServerTest
 			List<String> lines = FileUtil.getLines(in);
 			assertNotNull(lines);
 			assertEquals(3, lines.size());
+			assertEquals("0x01020304;1", lines.get(1));
+			assertEquals("0x02030405;2", lines.get(2));
 		}
 		finally
 		{
 			FileUtil.closeQuietely(in);
 		}
+		assertTrue(output.delete());
 	}
 }
