89b242776a34d301d4adab69085581f8453fa228
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2013-07-10 17:29:01 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@4019 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/db/postgres/PostgresTriggerReader.java
+++ src/workbench/db/postgres/PostgresTriggerReader.java
@@ -27,13 +27,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Savepoint;
+
+import workbench.log.LogMgr;
+
 import workbench.db.DefaultTriggerReader;
 import workbench.db.JdbcUtils;
 import workbench.db.NoConfigException;
 import workbench.db.ProcedureDefinition;
 import workbench.db.ProcedureReader;
 import workbench.db.TableIdentifier;
+import workbench.db.TriggerReader;
 import workbench.db.WbConnection;
+
+import workbench.storage.DataStore;
+import workbench.storage.SortDefinition;
+
 import workbench.util.SqlUtil;
 
 /**
@@ -45,16 +53,159 @@ import workbench.util.SqlUtil;
 public class PostgresTriggerReader
 	extends DefaultTriggerReader
 {
+	private boolean is93 = false;
 	public PostgresTriggerReader(WbConnection conn)
 	{
 		super(conn);
+		is93 = JdbcUtils.hasMinimumServerVersion(conn, "9.3");
+	}
 
+	@Override
+	public DataStore getTriggers(String catalog, String schema)
+		throws SQLException
+	{
+		DataStore result = super.getTriggers(catalog, schema);
+		if (is93)
+		{
+			retrieveEventTriggers(result);
+		}
+		return result;
+	}
+
+	private void retrieveEventTriggers(DataStore triggers)
+	{
+		String sql =
+			"select evtname as trigger,  \n" +
+			"       evtevent as event,  \n" +
+			"       obj_description(trg.oid, 'pg_event_trigger') as remarks \n" +
+			"FROM pg_event_trigger trg";
+
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		Savepoint sp = null;
+
+		int triggerCount = 0;
+		try
+		{
+			sp = dbConnection.setSavepoint();
+			stmt = dbConnection.getSqlConnection().prepareStatement(sql);
+			rs = stmt.executeQuery();
+
+			while (rs.next())
+			{
+				triggerCount ++;
+				String name = rs.getString(1);
+				String event = rs.getString(2);
+				String remarks = rs.getString(3);
+				int row = triggers.addRow();
+				triggers.setValue(row, TriggerReader.COLUMN_IDX_TABLE_TRIGGERLIST_TRG_NAME, name);
+				triggers.setValue(row, TriggerReader.COLUMN_IDX_TABLE_TRIGGERLIST_TRG_TYPE, "EVENT");
+				triggers.setValue(row, TriggerReader.COLUMN_IDX_TABLE_TRIGGERLIST_TRG_EVENT, event);
+				triggers.setValue(row, TriggerReader.COLUMN_IDX_TABLE_TRIGGERLIST_TRG_COMMENT, remarks);
+			}
+			dbConnection.releaseSavepoint(sp);
+		}
+		catch (Exception ex)
+		{
+			dbConnection.rollback(sp);
+			LogMgr.logWarning("PostgresTriggerReader.retrieveEventTriggers()", "Couldn not retrieve event triggers", ex);
+		}
+		finally
+		{
+			SqlUtil.closeAll(rs, stmt);
+		}
+
+		if (triggerCount > 0)
+		{
+			// sort the datastore again
+			SortDefinition def = new SortDefinition();
+			def.addSortColumn(0, true);
+			triggers.sort(def);
+		}
+		triggers.resetStatus();
+	}
+
+	@Override
+	public String getTriggerSource(String triggerCatalog, String triggerSchema, String triggerName, TableIdentifier triggerTable, String trgComment, boolean includeDependencies)
+		throws SQLException
+	{
+		if (triggerTable == null && is93)
+		{
+			return getEventTriggerSource(triggerName);
+		}
+		return super.getTriggerSource(triggerCatalog, triggerSchema, triggerName, triggerTable, trgComment, includeDependencies);
+	}
+
+	public String getEventTriggerSource(String triggerName)
+		throws SQLException
+	{
+		StringBuilder result = new StringBuilder(100);
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		Savepoint sp = null;
+
+		try
+		{
+			String funcName = null;
+			String event = null;
+			String funcSource = null;
+
+			sp = dbConnection.setSavepoint();
+			final String sql =
+				"select pr.proname, \n" +
+				"       trg.evtevent, \n " +
+				"       pg_get_functiondef(pr.oid) as func_source \n" +
+				"FROM pg_event_trigger trg \n" +
+				" JOIN pg_proc pr on pr.oid = trg.evtfoid \n" +
+				" join pg_namespace nsp on nsp.oid = pr.pronamespace \n" +
+				"where trg.evtname = ?";
+
+			stmt = dbConnection.getSqlConnection().prepareStatement(sql);
+			stmt.setString(1, triggerName);
+			rs = stmt.executeQuery();
+			if (rs.next())
+			{
+				funcName = rs.getString(1);
+				event = rs.getString(2);
+				funcSource = rs.getString(3);
+			}
+
+			result.append("DROP EVENT TRIGGER IF EXISTS ");
+			result.append(dbConnection.getMetadata().quoteObjectname(funcName));
+			result.append(" CASCADE;\n\n");
+			result.append("CREATE EVENT TRIGGER ");
+			result.append(dbConnection.getMetadata().quoteObjectname(funcName));
+			result.append("\n  ON ");
+			result.append(event);
+			result.append("\n  EXECUTE PROCEDURE ");
+			result.append(funcName);
+			result.append("();\n\nCOMMIT;\n");
+
+			result.append("\n---[ ");
+			result.append(funcName);
+			result.append(" ]---\n");
+			result.append(funcSource);
+			result.append('\n');
+
+			dbConnection.releaseSavepoint(sp);
+		}
+		catch (SQLException ex)
+		{
+			dbConnection.rollback(sp);
+			throw ex;
+		}
+		finally
+		{
+			SqlUtil.closeAll(rs, stmt);
+		}
+		return result.toString();
 	}
 
 	@Override
 	public CharSequence getDependentSource(String triggerCatalog, String triggerSchema, String triggerName, TableIdentifier triggerTable)
 		throws SQLException
 	{
+
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		String funcName = null;
