2c93cb111d599242e7d7850a590155cb20f1db00
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2013-08-01 06:48:17 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@4104 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- launcher/make32.cmd
+++ launcher/make32.cmd
@@ -1,6 +1,6 @@
 rcedit /C SQLWorkbench.exe
 rcedit /I SQLWorkbench.exe workbench.ico
-rcedit /N SQLWorkbench.exe sqlworkbench.ini
+rcedit /N SQLWorkbench.exe sqlworkbench32.ini
 
 rcedit /C sqlwbconsole.exe
 rcedit /I sqlwbconsole.exe console.ico
