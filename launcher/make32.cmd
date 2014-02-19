7b36076a038bb144c3a52fb4c2ddf314998ccfaa
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2014-02-19 16:07:37 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@4605 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- launcher/make32.cmd
+++ launcher/make32.cmd
@@ -2,33 +2,19 @@
 
 pushd "%~dp0"
 
-rem call :patch_ini
+echo main.class=workbench.WbStarter>t.ini
+copy /b t.ini+workbench.ini sqlworkbench.ini
 
 rcedit /C SQLWorkbench.exe
 rcedit /I SQLWorkbench.exe workbench.ico
 rcedit /N SQLWorkbench.exe sqlworkbench.ini
 
+echo main.class=workbench.console.SQLConsole>t.ini
+copy t.ini+workbench.ini sqlwbconsole.ini
+
 rcedit /C sqlwbconsole.exe
 rcedit /I sqlwbconsole.exe console.ico
 rcedit /N sqlwbconsole.exe sqlwbconsole.ini
 
-rem del sqlworkbench32.ini > nul
+del t.ini > nul
 
-goto :eof
-
-:patch_ini
-
-rem The vm.heapsize.max.percent property doesn't work correctly with 32bit JVMs if the computer has more than 3GB of memora
-rem (e.g. on a 64bit Windows but running 32bit Java)
-
-set file=sqlworkbench.ini
-set outfile=sqlworkbench32.ini
-echo.>%outfile%
-
-for /f " usebackq eol=# tokens=1 delims=" %%i in ("%file%") do (
-  if "%%i"=="vm.heapsize.max.percent=50" (
-     echo vm.heapsize.preferred=1024>>%outfile%
-  ) else (
-    @echo %%i>>%outfile%
-  )
-)
--- launcher/make64.cmd
+++ launcher/make64.cmd
@@ -1,9 +1,17 @@
 @echo off
 
+echo main.class=workbench.WbStarter>t.ini
+copy /b t.ini+workbench.ini sqlworkbench.ini > nul
+
 rcedit64 /C SQLWorkbench64.exe
 rcedit64 /I SQLWorkbench64.exe workbench.ico
 rcedit64 /N SQLWorkbench64.exe sqlworkbench.ini
 
+echo main.class=workbench.console.SQLConsole>t.ini
+copy t.ini+workbench.ini sqlwbconsole.ini > nul
+
 rcedit64 /C sqlwbconsole64.exe
 rcedit64 /I sqlwbconsole64.exe console.ico
 rcedit64 /N sqlwbconsole64.exe sqlwbconsole.ini
+
+del t.ini > nul
