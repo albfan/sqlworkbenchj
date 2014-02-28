cd0723c596354c33f3864b64ff2eccffe642a28f
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2014-02-28 14:09:39 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@4639 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- scripts/sqlworkbench.cmd
+++ scripts/sqlworkbench.cmd
@@ -2,17 +2,17 @@
 
 setlocal 
 
-if exist "%~dp0jre\bin\java.exe" set JAVA_HOME=%~dp0jre
-if not "%WORKBENCH_JDK%"=="" set JAVA_HOME=%WORKBENCH_JDK%
-if not "%JAVA_HOME%"=="" set JAVA_BINPATH=%JAVA_HOME%\bin\
+if exist "%~dp0jre\bin\java.exe" set JAVA_BINPATH=%~dp0jre\bin
+if exist "%WORKBENCH_JDK%\bin\java.exe" set JAVA_BINPATH=%WORKBENCH_JDK%\bin
+if exist "%JAVA_HOME%\bin\java.exe" set JAVA_BINPATH=%JAVA_HOME%\bin
 
 set wbdir=%~dp0
 
 if "%1"=="console" goto console_mode
 
 :gui
-start "SQL Workbench/J" "%JAVA_BINPATH%javaw"^
-      -Xmx256m ^
+start "SQL Workbench/J" "%JAVA_BINPATH%\javaw.exe"^
+      -Xmx512m ^
       -Dvisualvm.display.name=SQLWorkbench ^
       -Dsun.awt.keepWorkingSetOnMinimize=true ^
       -jar %wbdir%sqlworkbench.jar %*
@@ -21,8 +21,8 @@ goto :eof
 :console_mode
 title SQL Workbench/J
 set cp=%wbdir%sqlworkbench.jar;%wbdir%poi.jar
-"%JAVA_BINPATH%java" -Dvisualvm.display.name=SQLWorkbench ^
-                     -Xmx256m -cp %cp% workbench.console.SQLConsole %*
+"%JAVA_BINPATH%java.exe" -Dvisualvm.display.name=SQLWorkbench ^
+                         -Xmx512m -cp %cp% workbench.console.SQLConsole %*
 
 goto :eof
 
