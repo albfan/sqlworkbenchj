2f9ea67758d5fc03e785ab1f1bec47bcdca7a224
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2014-03-01 09:40:30 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@4642 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- scripts/dbexplorer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-# Start the SQL Workbench/J Database Explorer directly
-scriptpath=`dirname $0`
-$scriptpath/sqlworkbench.sh -dbexplorer $@
--- scripts/sqlwbconsole.cmd
+++ scripts/sqlwbconsole.cmd
@@ -1,13 +1,30 @@
 @echo off
 title SQL Workbench/J
 
-setlocal 
+set JAVA_BINPATH=
 
-if exist "%~dp0jre\bin\java.exe" set JAVA_HOME=%~dp0jre
-if not "%WORKBENCH_JDK%"=="" set JAVA_HOME=%WORKBENCH_JDK%
-if not "%JAVA_HOME%"=="" set JAVA_BINPATH=%JAVA_HOME%\bin\
+if exist "%~dp0jre\bin\java.exe" (
+   set JAVA_BINPATH=%~dp0jre\bin\
+) else (
+   if exist "%WORKBENCH_JDK%\bin\java.exe" (
+     set JAVA_BINPATH=%WORKBENCH_JDK%\bin\
+   ) else (
+     if exist "%JAVA_HOME%\bin\java.exe" set JAVA_BINPATH=%JAVA_HOME%\bin\
+   )
+)
 
-set wbdir=%~dp0
-set cp=%wbdir%sqlworkbench.jar;%wbdir%poi.jar
+set cp=%wbdir%sqlworkbench.jar;
+set cp=%cp%;%wbdir%poi*.jar
+set cp=%cp%;%wbdir%dom4j*.jar
+set cp=%cp%;%wbdir%stax*.jar
+set cp=%cp%;%wbdir%*odf*.jar
+set cp=%cp%;%wbdir%resolver*.jar
+set cp=%cp%;%wbdir%serializer*.jar
+set cp=%cp%;%wbdir%xerces*.jar
+set cp=%cp%;%wbdir%log4j.jar
+set cp=%cp%;%wbdir%mail.jar
 
-"%JAVA_BINPATH%java" -Xmx256m -Dvisualvm.display.name=SQLWorkbench -cp %cp% workbench.console.SQLConsole %*
+"%JAVA_BINPATH%java.exe" -Dvisualvm.display.name=SQLWorkbench ^
+                         -Xmx512m -cp %cp% workbench.console.SQLConsole %*
+
+
