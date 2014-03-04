f98e163c818ddc728fa1318a19fa92b817cfc6ce
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2014-03-04 17:10:39 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@4663 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

---
+++ test/dbtest/workbench/db/oracle/OracleFKHandlerTest.java
@@ -0,0 +1,89 @@
+/*
+ * OracleFKHandlerTest.java
+ *
+ * This file is part of SQL Workbench/J, http://www.sql-workbench.net
+ *
+ * Copyright 2002-2013, Thomas Kellerer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at.
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * To contact the author please send an email to: support@sql-workbench.net
+ *
+ */
+package workbench.db.oracle;
+
+import java.sql.SQLException;
+
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import workbench.TestUtil;
+import workbench.WbTestCase;
+import workbench.console.DataStorePrinter;
+import workbench.db.TableIdentifier;
+import workbench.db.WbConnection;
+import workbench.storage.DataStore;
+
+/**
+ *
+ * @author Thomas Kellerer
+ */
+public class OracleFKHandlerTest
+	extends WbTestCase
+{
+
+	public OracleFKHandlerTest()
+	{
+		super("OracleFKHandlerTest");
+	}
+
+	@AfterClass
+	public static void tearDown()
+		throws Exception
+	{
+		OracleTestUtil.cleanUpTestCase();
+	}
+
+	@Test
+	public void testHandler()
+		throws SQLException
+	{
+		WbConnection conn = OracleTestUtil.getOracleConnection();
+		assertNotNull("No Oracle connection available", conn);
+		OracleFKHandler fkHandler = new OracleFKHandler(conn);
+		String create =
+			"create table parent (id integer not null primary key);\n" +
+			"create table child_deferred (id integer not null primary key, pid integer not null,\n" +
+			" constraint fk_cd_parent foreign key (pid) references parent (id) deferrable initially deferred);\n" +
+			"create table child_immediate (id integer not null primary key, pid integer not null,\n" +
+			" constraint fk_ci_parent foreign key (pid) references parent (id) deferrable initially immediate);\n" +
+			"create table child_not_deferred (id integer not null primary key, pid integer not null,\n" +
+			" constraint fk_nd_parent foreign key (pid) references parent (id));\n";
+
+		TestUtil.executeScript(conn, create);
+
+		TableIdentifier parent = conn.getMetadata().findTable(new TableIdentifier("PARENt"));
+
+		DataStore fklist = fkHandler.getReferencedBy(parent);
+		assertNotNull(fklist);
+		assertEquals(3, fklist.getRowCount());
+
+		DataStorePrinter printer = new DataStorePrinter(fklist);
+		printer.printTo(System.out);
+
+		// fail("The test case is a prototype.");
+	}
+
+}
