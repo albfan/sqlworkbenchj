81ab274e51cc63c6f81498e5567cc453772f460b
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2015-02-04 09:23:29 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@5677 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- test/junit/workbench/sql/parser/ScriptParserTest.java
+++ test/junit/workbench/sql/parser/ScriptParserTest.java
@@ -815,13 +815,13 @@ public class ScriptParserTest
 
 		sql =
 			"set schema myschema;\n" +
-			"@install.sql\n" +
+			"@c:\\temp\\install.sql\n" +
 			"commit;";
-		p = new ScriptParser(ParserType.Standard);
+		p = new ScriptParser(ParserType.getTypeFromDBID("db2"));
 		p.setScript(sql);
 		assertEquals(3, p.getSize());
 		assertTrue(p.getCommand(0).startsWith("set schema myschema"));
-		assertTrue(p.getCommand(1).startsWith("@install.sql"));
+		assertTrue(p.getCommand(1).startsWith("@c:\\temp\\install.sql"));
 		assertTrue(p.getCommand(2).startsWith("commit"));
 
 		sql =
