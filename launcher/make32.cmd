5da170057a8fae0158c3851b919c0822792dc81f
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2015-02-22 22:05:32 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@5790 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- launcher/make32.cmd
+++ launcher/make32.cmd
@@ -4,20 +4,26 @@ pushd "%~dp0"
 
 echo main.class=workbench.WbStarter>t.ini
 echo vm.heapsize.preferred=1024>>t.ini
-copy /b t.ini+workbench.ini sqlworkbench.ini
+
+echo [ErrorMessages]>err.ini
+echo java.not.found=No suitable Java version could found. Please make sure you have a 32-bit Java 7 installed>>err.ini
+echo java.failed=Java failed to startup successfully. Please make sure you have 32-bit Java 7 installed>>err.ini
+
+copy /b t.ini+workbench.ini+err.ini sqlworkbench.ini
 
 rcedit /C SQLWorkbench.exe
 rcedit /I SQLWorkbench.exe workbench.ico
 rcedit /N SQLWorkbench.exe sqlworkbench.ini
 
 echo main.class=workbench.console.SQLConsole>t.ini
-copy t.ini+workbench.ini sqlwbconsole.ini
+copy t.ini+workbench.ini+err.ini sqlwbconsole.ini
 
 rcedit /C sqlwbconsole.exe
 rcedit /I sqlwbconsole.exe console.ico
 rcedit /N sqlwbconsole.exe sqlwbconsole.ini
 
 del t.ini > nul
+del err.ini > nul
 del sqlworkbench.ini > nul
 del sqlwbconsole.ini > nul
 
--- launcher/make64.cmd
+++ launcher/make64.cmd
@@ -1,20 +1,28 @@
 @echo off
 
+echo [ErrorMessages]>err.ini
+echo java.not.found=No suitable Java version could found. Please make sure you have a 64-bit Java 7 installed>>err.ini
+echo java.failed=Java failed to startup successfully. Please make sure you have 64-bit Java 7 installed>>err.ini
+
 echo main.class=workbench.WbStarter>t.ini
 echo vm.heapsize.max.percent=50>>t.ini
-copy /b t.ini+workbench.ini sqlworkbench.ini > nul
+
+copy /b t.ini+workbench.ini+err.ini sqlworkbench.ini > nul
 
 rcedit64 /C SQLWorkbench64.exe
 rcedit64 /I SQLWorkbench64.exe workbench.ico
 rcedit64 /N SQLWorkbench64.exe sqlworkbench.ini
 
 echo main.class=workbench.console.SQLConsole>t.ini
-copy t.ini+workbench.ini sqlwbconsole.ini > nul
+
+echo show.popup=false>>err.ini
+copy t.ini+workbench.ini+err.ini sqlwbconsole.ini > nul
 
 rcedit64 /C sqlwbconsole64.exe
 rcedit64 /I sqlwbconsole64.exe console.ico
 rcedit64 /N sqlwbconsole64.exe sqlwbconsole.ini
 
 del t.ini > nul
+del err.ini > nul
 del sqlworkbench.ini > nul
 del sqlwbconsole.ini > nul
