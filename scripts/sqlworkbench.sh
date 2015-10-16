494b23bab6e9e4e3a065fe657eb4b7922d7a9da6
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2015-10-16 10:51:36 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@6526 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- scripts/sqlworkbench.sh
+++ scripts/sqlworkbench.sh
@@ -1,26 +1,13 @@
 #!/bin/sh
 # Start SQL Workbench/J in GUI mode
 
-# resolve links - $0 may be a softlink
-PRG="$0"
-
-while [ -h "$PRG" ] ; do
-  ls=`ls -ld "$PRG"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    PRG="$link"
-  else
-    PRG=`dirname "$PRG"`/"$link"
-  fi
-done
-
-scriptpath=`dirname "$PRG"`
+SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
 
 JAVACMD="java"
 
-if [ -x "$scriptpath/jre/bin/java" ]
+if [ -x "$SCRIPT_PATH/jre/bin/java" ]
 then
-  JAVACMD="$scriptpath/jre/bin/java"
+  JAVACMD="$SCRIPT_PATH/jre/bin/java"
 elif [ -x "$WORKBENCH_JDK/bin/java" ]
 then
   JAVACMD="$WORKBENCH_JDK/bin/java"
@@ -32,16 +19,16 @@ then
   JAVACMD="$JAVA_HOME/bin/java"
 fi
 
-cp=$scriptpath/sqlworkbench.jar
-cp=$cp:$scriptpath/dom4j-1.6.1.jar
-cp=$cp:$scriptpath/poi-ooxml-schemas.jar
-cp=$cp:$scriptpath/poi-ooxml.jar
-cp=$cp:$scriptpath/poi.jar
-cp=$cp:$scriptpath/stax-api-1.0.1.jar
-cp=$cp:$scriptpath/resolver.jar
-cp=$cp:$scriptpath/serializer.jar
-cp=$cp:$scriptpath/simple-odf.jar
-cp=$cp:$scriptpath/ext/*
+cp=$SCRIPT_PATH/sqlworkbench.jar
+cp=$cp:$SCRIPT_PATH/dom4j-1.6.1.jar
+cp=$cp:$SCRIPT_PATH/poi-ooxml-schemas.jar
+cp=$cp:$SCRIPT_PATH/poi-ooxml.jar
+cp=$cp:$SCRIPT_PATH/poi.jar
+cp=$cp:$SCRIPT_PATH/stax-api-1.0.1.jar
+cp=$cp:$SCRIPT_PATH/resolver.jar
+cp=$cp:$SCRIPT_PATH/serializer.jar
+cp=$cp:$SCRIPT_PATH/simple-odf.jar
+cp=$cp:$SCRIPT_PATH/ext/*
 
 # When running in batch mode on a system with no X11 installed, the option
 #   -Djava.awt.headless=true
