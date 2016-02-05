57f21e125c4ae83b450b9639f0e7436cbb41db37
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2016-02-05 21:51:05 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@6778 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- test/dbtest/workbench/db/oracle/OracleArrayConverterTest.java
+++ test/dbtest/workbench/db/oracle/OracleArrayConverterTest.java
@@ -20,7 +20,24 @@
  */
 package workbench.db.oracle;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import workbench.TestUtil;
 import workbench.WbTestCase;
+
+import workbench.db.WbConnection;
+
+import workbench.storage.DataStore;
+
+import workbench.util.SqlUtil;
+
+import org.junit.AfterClass;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  *
@@ -34,32 +51,82 @@ public class OracleArrayConverterTest
     super("OracleArrayConverterTest");
   }
 
-  public void testVArray()
-  {
-    String sql =
-      "CREATE OR REPLACE TYPE number_list AS VARRAY(10) OF number;\n" +
-      "/\n" +
-      "CREATE TABLE varray_table (id number, numbers number_list);\n" +
-      "\n" +
-      "INSERT INTO varray_table VALUES (1, number_list(1,2,3));\n" +
-      "COMMIT;";
+	@BeforeClass
+	public static void setUpClass()
+		throws Exception
+	{
+		OracleTestUtil.initTestCase();
 
-    // output format WBJUNIT.NUMBER_LIST(1,2,3)
+		WbConnection con = OracleTestUtil.getOracleConnection();
+		Assume.assumeNotNull(con);
+
+    String sql =
+      "create or replace type number_list as varray(10) of number;\n" +
+      "/\n" +
+      "create table varray_table (numbers number_list);\n" +
+      "insert into varray_table values (number_list(1,2,3));\n" +
+      "commit;\n" +
+      "create table testgeo (shape  sdo_geometry);\n" +
+      "insert into testgeo values (sdo_geometry(2003,null,null,sdo_elem_info_array(1,1003,3),sdo_ordinate_array(1,1, 5,7)));\n" +
+      "commit;\n ";
+		TestUtil.executeScript(con, sql);
+	}
+
+	@AfterClass
+	public static void tearDownClass()
+		throws Exception
+	{
+		OracleTestUtil.cleanUpTestCase();
+	}
+
+  @Test
+  public void testVArray()
+    throws Exception
+  {
+		WbConnection con = OracleTestUtil.getOracleConnection();
+		assertNotNull(con);
+
+		String select = "SELECT numbers FROM varray_table";
+		Statement stmt = null;
+		ResultSet rs = null;
+    try
+    {
+      stmt = con.createStatement();
+      rs = stmt.executeQuery(select);
+      DataStore ds = new DataStore(rs, con, true);
+      assertEquals(1, ds.getRowCount());
+      String value = ds.getValueAsString(0, 0);
+      assertEquals(OracleTestUtil.SCHEMA_NAME + ".NUMBER_LIST(1,2,3)", value);
+    }
+    finally
+    {
+      SqlUtil.closeAll(rs, stmt);
+    }
   }
 
+  @Test
   public void testGeometry()
+    throws Exception
   {
-    String sql =
-      "CREATE TABLE testgeo\n" +
-      "(\n" +
-      "  id     NUMBER PRIMARY KEY,\n" +
-      "  shape  sdo_geometry\n" +
-      ");\n" +
-      "INSERT INTO testgeo \n" +
-      "  (id, shape) \n" +
-      "VALUES \n" +
-      "  (1,SDO_GEOMETRY(2003,NULL,NULL,SDO_ELEM_INFO_ARRAY(1,1003,3),SDO_ORDINATE_ARRAY(1,1, 5,7)));\n" +
-      "commit;\n ";
-    // output format: MDSYS.SDO_GEOMETRY(2003, NULL, NULL, MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,3), MDSYS.SDO_ORDINATE_ARRAY(1,1,5,7))
+		WbConnection con = OracleTestUtil.getOracleConnection();
+		assertNotNull(con);
+
+		String select = "SELECT shape FROM testgeo";
+		Statement stmt = null;
+		ResultSet rs = null;
+    try
+    {
+      stmt = con.createStatement();
+      rs = stmt.executeQuery(select);
+      DataStore ds = new DataStore(rs, con, true);
+      assertEquals(1, ds.getRowCount());
+      String value = ds.getValueAsString(0, 0);
+      assertEquals("MDSYS.SDO_GEOMETRY(2003, NULL, NULL, MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,3), MDSYS.SDO_ORDINATE_ARRAY(1,1,5,7))", value);
+    }
+    finally
+    {
+      SqlUtil.closeAll(rs, stmt);
+    }
+
   }
 }
