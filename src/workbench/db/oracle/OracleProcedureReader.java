6ad13979abe5bbf5efc0dfa441c02d790cd4eef3
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2017-06-20 18:48:44 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@7451 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/db/oracle/OracleProcedureReader.java
+++ src/workbench/db/oracle/OracleProcedureReader.java
@@ -423,22 +423,23 @@ public class OracleProcedureReader
       "           ap.procedure_name, \n" +
       "           ap.overload as overload_index, \n" +
       "           decode(ao.object_type, 'TYPE', 'OBJECT TYPE', ao.object_type) as remarks, \n" +
-      "           case  \n" +
-      "             when (select count(*)   \n" +
-      "                   from all_arguments aa   \n" +
-      "                   where aa.owner = ap.owner   \n" +
-      "                     and aa.package_name = ap.object_name  \n" +
-      "                     and aa.object_name = ap.procedure_name \n" +
-      "                       and aa.in_out = 'OUT' and aa.argument_name is null) = 1 then " + DatabaseMetaData.procedureReturnsResult + " \n" +
-      "               else " + DatabaseMetaData.procedureNoResult + " \n " +
-      "            end as procedure_type,  \n" +
-      "            ao.status,  \n" +
-      "            row_number() over (partition by ap.owner, ap.object_name, ap.procedure_name, ap.overload order by ao.object_type desc) as rn \n" +
+      "           decode(aa.anz, 1, " + DatabaseMetaData.procedureReturnsResult + ", " + DatabaseMetaData.procedureNoResult + " ) as procedure_type, \n" +
+      "           ao.status,  \n" +
+      "           row_number() over (partition by ap.owner, ap.object_name, ap.procedure_name, ap.overload order by ao.object_type desc) as rn \n" +
       "    from all_procedures ap \n" +
       "      join all_objects ao on ap.object_name = ao.object_name and ap.owner = ao.owner \n" +
+      "      left join (\n" +
+      "        select owner, package_name, object_name, count(*) as anz \n" +
+      "        from all_arguments \n" +
+      "        where in_out = 'OUT' \n" +
+      "          and argument_name is null \n" +
+      "        group by owner, package_name, object_name \n" +
+      "      ) aa on aa.owner = ap.owner \n" +
+      "          and aa.package_name = ap.object_name \n" +
+      "          and aa.object_name = ap.procedure_name \n" +
       "    where ao.object_type IN ('PACKAGE BODY', 'PACKAGE', 'TYPE', 'OBJECT TYPE') \n" +
       "      and ap.procedure_name is not null \n" +
-      "      and ap.object_name is not null\n" +
+      "      and ap.object_name    is not null \n" +
       "  )\n" +
       "  where rn = 1";
 
@@ -480,6 +481,7 @@ public class OracleProcedureReader
       LogMgr.logDebug("OracleProcedureReader.getProcedures()", "Using SQL to retrieve procedures:\n" + sql);
     }
 
+    long start = System.currentTimeMillis();
     Statement stmt = null;
     ResultSet rs = null;
     DataStore ds = buildProcedureListDataStore(this.connection.getMetadata(), false);
@@ -531,6 +533,8 @@ public class OracleProcedureReader
     {
       SqlUtil.closeAll(rs, stmt);
     }
+    long duration = System.currentTimeMillis() - start;
+    LogMgr.logDebug("OracleProcedureReader.getProcedures()", "Retrieving procedures took: " + duration + "ms");
     return ds;
   }
 
